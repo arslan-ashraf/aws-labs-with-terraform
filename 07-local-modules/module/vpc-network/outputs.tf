38eedd175ebea6a666357c3136474cb6d797ba26
arslan-ashraf <arslanash@gmail.com>
2026-05-02 10:34:11 -0400

    changes

--- 07-local-modules/module/vpc-network/outputs.tf
+++ 07-local-modules/module/vpc-network/outputs.tf
@@ -17,6 +17,27 @@ locals {
     # key takes on values like subnet_a, subnet_c
     for key in keys(local.public_subnets) : key => {
       subnet_id = aws_subnet.subnets_in_example_vpc[key].id
+      availability_zone = aws_subnet.subnets_in_example_vpc[key].availability_zone
     }
   }
+
+  output_private_subnets = {
+    for key in keys(local.private_subnets) : key => {
+      subnet_id = aws_subnet.subnets_in_example_vpc[key].id
+      availability_zone = aws_subnet.subnets_in_example_vpc[key].availability_zone
+    }
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.example_vpc.id
+  description = "The ID of the VPC."
+}
+
+output "public_subnets" {
+  value = local.output_public_subnets
+}
+
+output "private_subnets" {
+  value = local.output_private_subnets
 }
