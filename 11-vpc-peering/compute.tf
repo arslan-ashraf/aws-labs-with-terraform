691b34cafbd5bb17b19be6fa748e289de82aa3d9
arslan-ashraf <arslanash@gmail.com>
2026-05-09 15:23:11 -0400

    changes

--- 11-vpc-peering/compute.tf
+++ 11-vpc-peering/compute.tf
@@ -1,3 +1,52 @@
+#################################################################################
+########################## US-East-1 Region - EC2 ###############################
+#################################################################################
+
+data "aws_ami" "debian_ami" {
+  most_recent = true
+  owners      = ["136693071363"]
+
+  filter {
+    name = "name" # the AMI name field in AWS
+    # to find the string inside the values array, use:
+    # aws ec2 describe-images --region us-east-1 --image-ids ami-0b75f821522bcff85
+    values = ["debian-13-amd64-20260316-2418"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+
+  }
+
+}
+
+resource "aws_instance" "ec2_instances_US_east" {
+  ami                         = data.aws_ami.debian_ami.id
+  instance_type               = "t2.nano"
+  subnet_id                   = aws_subnet.subnet_in_US_east.id
+  availability_zone           = "us-east-1a"
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    aws_security_group.security_group_US_east.id
+  ]
+
+  tags = { Name = "debian-machine-in-US-east" }
+
+  # if the ami gets updated by the time the next apply happens, Terraform
+  # might delete the previous instance and replace it with the latest ami
+  # to prevent that:
+  lifecycle {
+    ignore_changes = [ami]
+  }
+}
+
+
+#################################################################################
+############################ Tokyo Region - EC2 #################################
+#################################################################################
+
 data "aws_ami" "ubuntu_ami" {
   most_recent = true
 
@@ -20,54 +69,18 @@ data "aws_ami" "ubuntu_ami" {
 
 }
 
-data "aws_ami" "debian_ami" {
-  most_recent = true
-  owners      = ["136693071363"]
-
-  filter {
-    name = "name" # the AMI name field in AWS
-    # to find the string inside the values array, use:
-    # aws ec2 describe-images --region us-east-1 --image-ids ami-0b75f821522bcff85
-    values = ["debian-13-amd64-20260316-2418"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-
-  }
-
-}
-
-
-locals {
-  ami_ids = {
-    ubuntu = data.aws_ami.ubuntu_ami.id
-    debian = data.aws_ami.debian_ami.id
-  }
-}
-
-# create multiple ec2 intstances with for_each loop
-resource "aws_instance" "create_instances_from_map" {
-  for_each = var.ec2_instance_config
-
-  # local.ami_ids map holds dynamically fetched ami ids and each.value.ami
-  # provides the keys to look into the local.ami_ids map
-  ami = local.ami_ids[each.value.ami]
-
-  instance_type               = each.value.instance_type
-  subnet_id                   = aws_subnet.subnets_in_example_vpc[each.value.subnet_name].id
-  availability_zone           = "us-east-1a"
-  associate_public_ip_address = each.value.subnet_name == "private_subnet" ? false : true
-  key_name                    = "key-for-ec2-connection"
+resource "aws_instance" "ec2_instances_Tokyo" {
+  ami                         = data.aws_ami.ubuntu_ami.id
+  instance_type               = "t2.nano"
+  subnet_id                   = aws_subnet.subnet_in_Tokyo.id
+  availability_zone           = "apne1-az1"
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [
-    aws_security_group.multiple_security_groups[each.value.security_group].id
+    aws_security_group.security_group_Tokyo.id
   ]
 
-  tags = {
-    Name = "${each.value.ami}-machine"
-  }
+  tags = { Name = "ubuntu-machine-in-Tokyo" }
 
   # if the ami gets updated by the time the next apply happens, Terraform
   # might delete the previous instance and replace it with the latest ami
@@ -75,9 +88,4 @@ resource "aws_instance" "create_instances_from_map" {
   lifecycle {
     ignore_changes = [ami]
   }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "key-for-ec2-connection"
-  public_key = file("~/.ssh/key-for-ec2-connection.pub")
 }
