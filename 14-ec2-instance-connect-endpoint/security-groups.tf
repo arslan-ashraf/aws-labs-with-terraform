5b49dd982787129e5d7ca8da94b02a90e763996a
arslan-ashraf <arslanash@gmail.com>
2026-05-15 17:49:52 -0400

    changes

--- 14-ec2-instance-connect-endpoint/security-groups.tf
+++ 14-ec2-instance-connect-endpoint/security-groups.tf
@@ -1,8 +1,24 @@
+# this file has two security groups, one with an ingress ssh rule and the other
+# with a egress ssh rule
+
 resource "aws_security_group" "security_group_for_ec2_instance" {
   name   = "security_group_for_ec2_instance"
   vpc_id = aws_vpc.example_vpc.id
   tags   = { Name = "security_group_for_ec2_instance" }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
+  security_group_id = aws_security_group.security_group_public_traffic.id
+  
+  # where is the traffic coming from
+  cidr_ipv4 = aws_security_group.private_subnet_for_ec2_instance_endpoint.cidr_ipv4
+  from_port = 22
+  to_port   = 22
+
+  ip_protocol = "tcp"
+}
+
+
 
 resource "aws_security_group" "security_group_for_ec2_instance_endpoint" {
   name   = "security_group_for_ec2_instance_endpoint"
@@ -10,12 +26,13 @@ resource "aws_security_group" "security_group_for_ec2_instance_endpoint" {
   tags   = { Name = "security_group_for_ec2_instance_endpoint" }
 }
 
-resource "aws_vpc_security_group_egress_rule" "egress_ping_rule" {
+resource "aws_vpc_security_group_egress_rule" "egress_ssh_rule" {
   security_group_id = aws_security_group.security_group_public_traffic.id
-  cidr_ipv4         = "0.0.0.0/0" # where is the traffic going
 
-  from_port = 8
-  to_port   = 0
+  # where is the traffic going
+  cidr_ipv4 = aws_subnet.private_subnet_for_ec2_instance.cidr_ipv4 
+  from_port = 22
+  to_port   = 22
 
-  ip_protocol = "icmp"
+  ip_protocol = "tcp"
 }
