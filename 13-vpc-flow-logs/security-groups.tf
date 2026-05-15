9c79f2f9484289c0dc88c0ae62171e1a6e17301d
arslan-ashraf <arslanash@gmail.com>
2026-05-15 17:42:10 -0400

    changes

---
+++ 13-vpc-flow-logs/security-groups.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "security_group_public_traffic" {
+  name   = "security-group-for-public-traffic"
+  vpc_id = aws_vpc.example_vpc.id
+  tags   = { Name = "sg-for-public-traffic" }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_http_traffic_rule" {
+  security_group_id = aws_security_group.security_group_public_traffic.id
+  cidr_ipv4         = "0.0.0.0/0" # where is the traffic coming from
+
+  # to allow ingress traffic for ssh
+  from_port = 22
+  to_port   = 22
+
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "engress_ping_rule" {
+  security_group_id = aws_security_group.security_group_public_traffic.id
+  cidr_ipv4         = "0.0.0.0/0" # where is the traffic going
+
+  from_port = 8
+  to_port   = 0
+
+  ip_protocol = "icmp"
+}
