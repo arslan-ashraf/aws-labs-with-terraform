e1bc2f42ed6a66be946e79c54c2905a944c31e2f
arslan-ashraf <arslanash@gmail.com>
2026-05-16 18:37:48 -0400

    changes

--- 16-vpc-interface-endpoint/security-groups.tf
+++ 16-vpc-interface-endpoint/security-groups.tf
@@ -38,8 +38,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_egress_https_to_s3" {
 }
 
 
-
-
 resource "aws_security_group" "security_group_for_ec2_instance_endpoint" {
   name   = "security_group_for_ec2_instance_endpoint"
   vpc_id = aws_vpc.example_vpc.id
