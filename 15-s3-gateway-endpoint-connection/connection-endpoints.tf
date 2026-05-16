c57ed35d9592ea299b2020e41bf4e962d1355a7a
arslan-ashraf <arslanash@gmail.com>
2026-05-15 21:47:40 -0400

    changes

---
+++ 15-s3-gateway-endpoint-connection/connection-endpoints.tf
@@ -0,0 +1,48 @@
+resource "aws_ec2_instance_connect_endpoint" "instance_connect_endpoint" {
+  subnet_id          = aws_subnet.private_subnet_for_ec2_instance_endpoint.id
+  security_group_ids = [aws_security_group.security_group_for_ec2_instance_endpoint.id]
+  
+  tags = { Name = "instance_connect_endpoint" }
+}
+
+
+
+
+# 1. Look up the S3 service name for your current region
+data "aws_vpc_endpoint_service" "s3_endpoint" {
+  service      = "s3"
+  service_type = "Gateway"
+}
+
+# 2. Create the S3 Gateway VPC Endpoint
+resource "aws_vpc_endpoint" "s3_gateway" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = data.aws_vpc_endpoint_service.s3_endpoint.service_name
+  vpc_endpoint_type = "Gateway"
+
+  # Automatically add S3 routes to these route tables
+  route_table_ids = [aws_route_table.private.id]
+
+  # Optional: Define an access policy (defaults to Full Access if omitted)
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowSpecificS3Actions"
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = ["${aws_s3_bucket.example_bucket.arn}:*"]
+        Action    = [
+          "s3:GetObject", 
+          "s3:PutObject", 
+          "s3:ListBucket", 
+          "s3:DeleteObject"
+        ]
+      }
+    ]
+  })
+
+  tags = {
+    Name = "s3-gateway-endpoint"
+  }
+}
