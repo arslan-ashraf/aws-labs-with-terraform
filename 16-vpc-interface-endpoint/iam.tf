8602cefc989f6f5b9a406c8022540b8ca59f0c34
arslan-ashraf <arslanash@gmail.com>
2026-05-16 20:39:08 -0400

    changes

--- 16-vpc-interface-endpoint/iam.tf
+++ 16-vpc-interface-endpoint/iam.tf
@@ -1,3 +1,7 @@
+#########################################################################
+############################# EC2 IAM ROLE ##############################
+#########################################################################
+
 data "aws_iam_policy_document" "ec2_assume_role_document" {
   statement {
     effect = "Allow"
@@ -17,6 +21,10 @@ resource "aws_iam_role" "ec2_sqs_access_role" {
 }
 
 
+#########################################################################
+############################ EC2 IAM POLICY #############################
+#########################################################################
+
 data "aws_iam_policy_document" "ec2_sqs_access_permissions" {
   statement {
     effect    = "Allow"
@@ -34,17 +42,21 @@ data "aws_iam_policy_document" "ec2_sqs_access_permissions" {
 
 # here we create the policy that will take on persmissions defined
 # in the JSON document imported by aws_iam_policy_document.lambda_permissions
-resource "aws_iam_policy" "sqs_bucket_policy" {
-  policy = data.aws_iam_policy_document.ec2_s3_access_permissions.json
-  name   = "s3_bucket_policy"
+resource "aws_iam_policy" "sqs_policy" {
+  policy = data.aws_iam_policy_document.ec2_sqs_access_permissions.json
+  name   = "sQS_policy"
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_s3_role_policy_attachment" {
   role       = aws_iam_role.ec2_sqs_access_role.name
-  policy_arn = aws_iam_policy.s3_bucket_policy.arn
+  policy_arn = aws_iam_policy.sqs_policy.arn
 }
+
+#########################################################################
+####################### EC2 IAM INSTANCE PROFILE ########################
+#########################################################################
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "my_ec2_instance_profile"
-  role = aws_iam_role.ec2_s3_access_role.name
+  role = aws_iam_role.ec2_sqs_access_role.name
 }
