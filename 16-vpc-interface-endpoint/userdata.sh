57ab725b0e2834b761a633ea95d3f129853e5088
arslan-ashraf <arslanash@gmail.com>
2026-05-16 20:59:38 -0400

    changes

--- 16-vpc-interface-endpoint/userdata.sh
+++ 16-vpc-interface-endpoint/userdata.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 echo "working"
+
+: <<'COMMENT'
+
+#!/bin/bash
+# Update package list and install dependencies
+sudo apt-get update -y
+sudo apt-get install -y unzip curl
+
+# Download the official AWS CLI v2 installer
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Unzip and run the installer
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Optional: Clean up installation files
+rm -rf awscliv2.zip ./aws
+
+COMMENT
