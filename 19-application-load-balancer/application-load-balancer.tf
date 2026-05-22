f96436af1ad05ff3286ed5ec3b072f98f0d2b14f
arslan-ashraf <arslanash@gmail.com>
2026-05-22 18:51:03 -0400

    changes

---
+++ 19-application-load-balancer/application-load-balancer.tf
@@ -0,0 +1,14 @@
+resource "aws_alb" "aws_alb_suffix" {
+  name_prefix = "Optional"
+  internal = "Optional"
+  load_balancer_type = "Optional"
+  security_groups = "Optional"
+  access_logs = "Optional"
+  subnets = "Optional"
+  subnet_mapping = "Optional"
+  idle_timeout = "Optional"
+  enable_deletion_protection = "Optional"
+  enable_http2 = "Optional"
+  ip_address_type = "Optional"
+  tags = "Optional"
+}
