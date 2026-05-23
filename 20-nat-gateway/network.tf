b16d6c968d41fc03822742eedd687d6d40158f17
arslan-ashraf <arslanash@gmail.com>
2026-05-22 20:50:33 -0400

    changes

--- 20-nat-gateway/network.tf
+++ 20-nat-gateway/network.tf
@@ -8,6 +8,9 @@ resource "aws_internet_gateway" "internet_gateway_for_example_vpc" {
   tags   = { Name = "internet_gateway_for_example_vpc" }
 }
 
+#########################################################################
+############## PUBLIC SUBNET & ROUTE TABLE FOR NAT GATEWAY ##############
+#########################################################################
 
 resource "aws_subnet" "public_subnet_for_nat_gateway" {
   availability_zone = "us-east-1a"
@@ -17,6 +20,23 @@ resource "aws_subnet" "public_subnet_for_nat_gateway" {
 
   tags = { Name = "public_subnet_for_nat_gateway" }
 }
+
+resource "aws_route_table" "route_table_for_public_subnet_in_example_vpc" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway_for_example_vpc.id
+  }
+
+  tags = { Name = "route_table_for_public_subnet_in_example_vpc" }
+
+}
+
+
+#########################################################################
+################## PRIVATE SUBNET & ROUTE TABLE FOR EC2 #################
+#########################################################################
 
 
 resource "aws_subnet" "private_subnet_for_ec2_instance" {
