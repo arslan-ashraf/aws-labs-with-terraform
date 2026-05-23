a7a87919ecb157103c6c1602d523d3b6cd6bc550
arslan-ashraf <arslanash@gmail.com>
2026-05-23 16:00:01 -0400

    changes

--- 19-application-load-balancer/compute.tf
+++ 19-application-load-balancer/compute.tf
@@ -3,10 +3,9 @@ resource "aws_instance" "ec2_instance" {
   region                      = "us-east-1"
   availability_zone           = "us-east-1a"
   instance_type               = "t2.nano"
-  associate_public_ip_address = false
-  subnet_id                   = aws_subnet.private_subnet_for_ec2_instance.id
-  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
-  # user_data                   = file("${path.module}/userdata.sh")
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  user_data                   = file("${path.module}/userdata.sh")
 
   vpc_security_group_ids = [
     aws_security_group.security_group_for_ec2_instance.id
