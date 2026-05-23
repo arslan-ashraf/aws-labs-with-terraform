7757b6cea5764b51248dda37a4026fe0574d2b87
arslan-ashraf <arslanash@gmail.com>
2026-05-22 20:54:02 -0400

    changes

---
+++ 20-nat-gateway/nat-gateway.tf
@@ -0,0 +1,18 @@
+# allocate elastic IP (EIP) for the nat gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway_for_example_vpc]
+
+  tags = { Name = "nat_gateway_eip" }
+}
+
+# 6. Create the NAT Gateway in the Public Subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet_for_nat_gateway.id
+
+  tags = { Name = "nat_gateway" }
+
+  # Explicit dependency to ensure proper ordering during creation/destruction
+  depends_on = [aws_internet_gateway.internet_gateway_for_example_vpc]
+}
