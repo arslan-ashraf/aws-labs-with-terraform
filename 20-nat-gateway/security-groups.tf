fe0ebcabf995b60f34930f04fe4ac2e1ee17e316
arslan-ashraf <arslanash@gmail.com>
2026-05-22 22:29:14 -0400

    changes

--- 20-nat-gateway/security-groups.tf
+++ 20-nat-gateway/security-groups.tf
@@ -8,11 +8,25 @@ resource "aws_security_group" "security_group_for_ec2_instance" {
   tags   = { Name = "security_group_for_ec2_instance" }
 }
 
+# allow SSH traffic in from the EC2 instance connect endpoint
 resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
   security_group_id = aws_security_group.security_group_for_ec2_instance.id
   
   # where is the traffic coming from
   referenced_security_group_id = aws_security_group.security_group_for_ec2_instance_endpoint.id
+
+  from_port = 22
+  to_port   = 22
+
+  ip_protocol = "tcp"
+}
+
+# allow traffic out to the NAT gateway
+resource "aws_vpc_security_group_egress_rule" "egress_nat_gateway_rule" {
+  security_group_id = aws_security_group.security_group_for_ec2_instance.id
+
+  # Target destination
+  cidr_ipv4 = "0.0.0.0/0"
 
   from_port = 22
   to_port   = 22
@@ -32,6 +46,7 @@ resource "aws_security_group" "security_group_for_ec2_instance_endpoint" {
   tags   = { Name = "security_group_for_ec2_instance_endpoint" }
 }
 
+# send SSH traffic out to the EC2 instance
 resource "aws_vpc_security_group_egress_rule" "egress_ssh_rule" {
   security_group_id = aws_security_group.security_group_for_ec2_instance_endpoint.id
 
