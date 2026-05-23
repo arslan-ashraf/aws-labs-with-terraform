8167f1a76c46b8a3c5dc9ed973c19730e8952d9b
arslan-ashraf <arslanash@gmail.com>
2026-05-23 15:47:00 -0400

    changes

--- 19-application-load-balancer/network.tf
+++ 19-application-load-balancer/network.tf
@@ -8,6 +8,8 @@ resource "aws_internet_gateway" "internet_gateway_for_example_vpc" {
   tags = { Name = "internet_gateway_for_example_vpc" }
 }
 
+
+
 resource "aws_subnet" "public_subnet_1_in_example_vpc" {
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
@@ -25,12 +27,37 @@ resource "aws_route_table" "route_table_for_public_subnet_1_in_example_vpc" {
     gateway_id = aws_internet_gateway.internet_gateway_for_example_vpc.id
   }
 
-  tags = { Name = "route_table_for_public_subnet_in_example_vpc" }
+  tags = { Name = "route_table_for_public_subnet_1_in_example_vpc" }
 
 }
 
-resource "aws_route_table_association" "route_table_association_public_subnet_example_vpc" {
-  subnet_id      = aws_subnet.public_subnet_in_example_vpc.id
-  route_table_id = aws_route_table.route_table_for_public_subnet_in_example_vpc.id
+resource "aws_route_table_association" "route_table_association_public_subnet_1_example_vpc" {
+  subnet_id      = aws_subnet.public_subnet_1_in_example_vpc.id
+  route_table_id = aws_route_table.route_table_for_public_subnet_1_in_example_vpc.id
 }
 
+resource "aws_subnet" "public_subnet_2_in_example_vpc" {
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.2.0/24"
+  vpc_id            = aws_vpc.example_vpc.id
+
+  tags = { Name = "public_subnet_2_in_example_vpc" }
+  
+}
+
+resource "aws_route_table" "route_table_for_public_subnet_2_in_example_vpc" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway_for_example_vpc.id
+  }
+
+  tags = { Name = "route_table_for_public_subnet_2_in_example_vpc" }
+
+}
+
+resource "aws_route_table_association" "route_table_association_public_subnet_2_example_vpc" {
+  subnet_id      = aws_subnet.public_subnet_2_in_example_vpc.id
+  route_table_id = aws_route_table.route_table_for_public_subnet_2_in_example_vpc.id
+}
