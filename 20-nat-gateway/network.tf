70364267eab0294973689bbe7307b289a882434e
arslan-ashraf <arslanash@gmail.com>
2026-05-22 20:25:49 -0400

    changes

--- 20-nat-gateway/network.tf
+++ 20-nat-gateway/network.tf
@@ -8,7 +8,6 @@ resource "aws_internet_gateway" "internet_gateway_for_example_vpc" {
   tags   = { Name = "internet_gateway_for_example_vpc" }
 }
 
-
 resource "aws_subnet" "private_subnet_for_ec2_instance" {
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.5.0/24"
@@ -17,6 +16,24 @@ resource "aws_subnet" "private_subnet_for_ec2_instance" {
   tags = { Name = "private_subnet_for_ec2_instance" }
 }
 
+resource "aws_route_table" "route_table_for_private_subnet_in_example_vpc" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = { Name = "route_table_for_private_subnet_in_example_vpc" }
+
+}
+
+# attach the route table to one of the subnets to make it public
+# note: a subnet can only be attached to a single route table
+resource "aws_route_table_association" "route_table_association_public_subnet_example_vpc" {
+  subnet_id      = aws_subnet.private_subnet_for_ec2_instance.id
+  route_table_id = aws_route_table.route_table_for_private_subnet_in_example_vpc.id
+}
 
 
 resource "aws_subnet" "public_subnet_for_nat_gateway" {
