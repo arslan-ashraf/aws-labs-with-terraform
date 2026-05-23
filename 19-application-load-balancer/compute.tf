6f22bf6711915fb998e0a68f29d406ab6ae02636
arslan-ashraf <arslanash@gmail.com>
2026-05-23 17:09:13 -0400

    changes

--- 19-application-load-balancer/compute.tf
+++ 19-application-load-balancer/compute.tf
@@ -14,3 +14,20 @@ resource "aws_instance" "ec2_instance" {
   tags = { Name = "amazon-linux-ami" }
 
 }
+
+resource "aws_instance" "ec2_instance" {
+  ami                         = "ami-0236922087fa98b6e"
+  region                      = "us-east-1"
+  availability_zone           = "us-east-1b"
+  instance_type               = "t2.nano"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_subnet_2.id
+  user_data                   = file("${path.module}/userdata.sh")
+
+  vpc_security_group_ids = [
+    aws_security_group.security_group_for_ec2_instance.id
+  ]
+
+  tags = { Name = "amazon-linux-ami" }
+
+}
