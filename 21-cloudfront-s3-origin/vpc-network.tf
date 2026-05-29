144ccc7103a607488ff76c889493f7e3941a8a81
arslan-ashraf <arslanash@gmail.com>
2026-05-29 14:25:17 -0400

    changes

--- 21-cloudfront-s3-origin/vpc-network.tf
+++ 21-cloudfront-s3-origin/vpc-network.tf
@@ -2,32 +2,3 @@ resource "aws_vpc" "example_vpc" {
   cidr_block = "10.0.0.0/16"
   tags       = { Name = "example_vpc" }
 }
-
-resource "aws_subnet" "public_subnet_in_example_vpc" {
-  availability_zone = "us-east-1a"
-  cidr_block        = "10.0.5.0/24"
-  vpc_id            = aws_vpc.example_vpc.id
-
-  tags = { Name = "public_subnet_in_example_vpc" }
-}
-
-# create a route table and attach it to the internet gateway for all
-# inbound and outbound traffic
-resource "aws_route_table" "route_table_for_public_subnet_in_example_vpc" {
-  vpc_id = aws_vpc.example_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway_for_example_vpc.id
-  }
-
-  tags = { Name = "route_table_for_public_subnet_in_example_vpc" }
-
-}
-
-# attach the route table to one of the subnets to make it public
-# note: a subnet can only be attached to a single route table
-resource "aws_route_table_association" "route_table_association_public_subnet_example_vpc" {
-  subnet_id      = aws_subnet.public_subnet_in_example_vpc.id
-  route_table_id = aws_route_table.route_table_for_public_subnet_in_example_vpc.id
-}
