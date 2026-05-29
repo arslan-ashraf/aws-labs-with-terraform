e92380fa49ffd758bce42a249b7edf59ffd59193
arslan-ashraf <arslanash@gmail.com>
2026-05-29 15:43:03 -0400

    changes

--- 22-cloudfront-s3-ec2-origin-group/static-files-s3-bucket.tf
+++ 22-cloudfront-s3-ec2-origin-group/static-files-s3-bucket.tf
@@ -28,15 +28,15 @@ resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
 resource "aws_s3_object" "index_html_page" {
   bucket       = aws_s3_bucket.static_files_s3_bucket.id
   key          = "index.html"
-  source       = "index.html"
+  source       = "${path.module}/static-files/index.html"
   content_type = "text/html"
-  etag         = filemd5("index.html")
+  etag         = filemd5("${path.module}/static-files/index.html")
 }
 
 resource "aws_s3_object" "error_html_page" {
   bucket       = aws_s3_bucket.static_files_s3_bucket.id
   key          = "error.html"
-  source       = "error.html"
+  source       = "${path.module}/static-files/error.html"
   content_type = "text/html"
-  etag         = filemd5("error.html")
+  etag         = filemd5("${path.module}/static-files/error.html")
 }
