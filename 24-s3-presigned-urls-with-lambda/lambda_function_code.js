d38f7d5fe0b674d3fcfe197ab302d89206328dd3
arslan-ashraf <arslanash@gmail.com>
2026-06-05 17:51:07 -0400

    changes

--- 24-s3-presigned-urls-with-lambda/lambda_function_code.js
+++ 24-s3-presigned-urls-with-lambda/lambda_function_code.js
@@ -4,19 +4,21 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 export const handler = async (event) => {
     const s3Client = new S3Client({ region: "us-east-1" });
     
-    const bucketName = "your-s3-bucket-name";
-    const objectKey = "example-file.pdf";
+    const bucket_name = "private-bucket-49587sdf90458s3";
+    
+    // this must be the name of the file to upload and download
+    const object_key = "example-file.txt";
 
     try {
         // Create the command (use PutObjectCommand for file uploads)
         const command = new GetObjectCommand({
-            Bucket: bucketName,
-            Key: objectKey,
+            Bucket: bucket_name,
+            Key: object_key,
         });
 
         // Generate the presigned URL
         const presignedUrl = await getSignedUrl(s3Client, command, { 
-            expiresIn: 3600 // URL expires in 1 hour (in seconds)
+            expiresIn: 300 // URL expires in 5 minutes (300 seconds)
         });
 
         return {
