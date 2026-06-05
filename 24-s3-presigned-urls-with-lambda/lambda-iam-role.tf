7d3e22fadd08b0bce30a6387133b15d665315bbf
arslan-ashraf <arslanash@gmail.com>
2026-06-05 15:18:50 -0400

    changes

--- 24-s3-presigned-urls-with-lambda/lambda-iam-role.tf
+++ 24-s3-presigned-urls-with-lambda/lambda-iam-role.tf
@@ -18,17 +18,19 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_policy_document.json
 }
 
-# IAM Policy to allow Lambda to issue PutObject/GetObject actions
-resource "aws_iam_role_policy" "lambda_s3_policy" {
-  name = "lambda_s3_policy"
-  role = aws_iam_role.lambda_role.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action   = ["s3:PutObject", "s3:GetObject"]
-      Effect   = "Allow"
-      Resource = "${aws_s3_bucket.secure_bucket.arn}/*"
-    }]
-  })
+# IAM Policy to allow Lambda to issue PutObject/GetObject actions
+data "aws_iam_policy_document" "lambda_s3_permissions" {
+  statement {
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.secure_bucket.arn}/*"]
+    actions = ["s3:PutObject", "s3:GetObject"]
+  }
+}
+
+
+
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name = "lambda_s3_policy"
+  policy = data.aws_iam_policy_document.lambda_s3_permissions.json
 }
