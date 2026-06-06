a5f59c956a27b6d02df0f05a68c7654dc1a893d4
arslan-ashraf <arslanash@gmail.com>
2026-06-06 16:16:54 -0400

    changes

--- 25-s3-presigned-urls-with-lambda-secure/use-presigned-url.sh
+++ 25-s3-presigned-urls-with-lambda-secure/use-presigned-url.sh
@@ -13,3 +13,15 @@ curl -v "<presigned_url>" \
 -F "X-Amz-Security-Token=..."  \
 -F "file=@./file_to_upload_to_s3.json"
 COMMENT
+
+curl -v "<presigned_url>" \
+-F "key=..."  \
+-F "X-Amz-Algorithm=..."  \
+-F "X-Amz-Credential=..."  \
+-F "X-Amz-Date=..."  \
+-F "Policy=..."  \
+-F "X-Amz-Signature=..."  \
+-F "x-amz-meta-ip=..." \
+-F "bucket=..."  \
+-F "X-Amz-Security-Token=..."  \
+-F "file=@./file_to_upload_to_s3.json"
