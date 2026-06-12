bb4d8e375e1c9abe4bc58b95be51b736ceb20215
arslan-ashraf <arslanash@gmail.com>
2026-06-12 15:44:26 -0400

    changes

--- 26-basic-eks-cluster-with-module-pod-s3-access/main.tf
+++ 26-basic-eks-cluster-with-module-pod-s3-access/main.tf
@@ -1,65 +1,66 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs = slice(data.aws_availability_zones.available.names, 0, 1)
+}
+
+######################################
+VPC
+######################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.cluster_name}-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = local.azs
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["10.0.1.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
 #######################################
-# VPC
+# EKS
 #######################################
 
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "~> 5.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
 
-#   name = "${var.cluster_name}-vpc"
+  name               = var.cluster_name
+  kubernetes_version = "1.33"
 
-#   cidr = "10.0.0.0/16"
+  endpoint_public_access = true
 
-#   azs             = local.azs
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-#   enable_nat_gateway = true
-#   single_nat_gateway = true
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
 
-#   public_subnet_tags = {
-#     "kubernetes.io/role/elb" = 1
-#   }
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
 
-#   private_subnet_tags = {
-#     "kubernetes.io/role/internal-elb" = 1
-#   }
-# }
-
-# #######################################
-# # EKS
-# #######################################
-
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 21.0"
-
-#   name               = var.cluster_name
-#   kubernetes_version = "1.33"
-
-#   endpoint_public_access = true
-
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-
-#   eks_managed_node_groups = {
-#     default = {
-#       instance_types = ["t3.medium"]
-
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 2
-#     }
-#   }
-
-#   tags = {
-#     Environment = "learning"
-#     Terraform   = "true"
-#   }
-# }
+  tags = {
+    Environment = "learning"
+    Terraform   = "true"
+  }
+}
