6cd36876b4d7cfa03d347655212a0e73cd8ffb1c
arslan-ashraf <arslanash@gmail.com>
2026-06-13 09:58:28 -0400

    changes

---
+++ 26-basic-eks-cluster-with-module-pod-s3-access/pod-identity-agent.tf
@@ -0,0 +1,12 @@
+# this resource can be used if the EKS cluster is launched without 
+# the EKS module, if using the EKS module, the add on can be encoded
+# within the EKS module block
+
+
+# resource "aws_eks_addon" "pod_identity" {
+#   cluster_name  = "your-eks-cluster-name" # Replace with your cluster name
+#   addon_name    = "eks-pod-identity-agent"
+#   addon_version = "v1.3.0-eksbuild.1"     # Use the latest version for your K8s release
+
+#   resolve_conflicts_on_update = "OVERWRITE"
+# }
