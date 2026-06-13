4b6a75d5555d78cb73f502ffb320e27374ca5e19
arslan-ashraf <arslanash@gmail.com>
2026-06-13 18:04:31 -0400

    changes

--- 27-sns-cloudwatch-alarm-ec2-cpu-spike/cloudwatch-alarm.tf
+++ 27-sns-cloudwatch-alarm-ec2-cpu-spike/cloudwatch-alarm.tf
@@ -1,29 +1,24 @@
-resource "aws_cloudwatch_log_group" "ec2_metrics_log_group" {
-  name = "ec2_metrics_log_group"
-  retention_in_days = 30
-}
-
 resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "high-cpu-${aws_instance.web.id}"
+  alarm_name          = "high-cpu-${aws_instance.ec2_instance.id}"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
+  evaluation_interval = 60 # frequency in seconds at which alarm is evaluated
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = 300
-  statistic           = "Average"
+  period              = 60
+  statistic           = "Maximum"
   threshold           = var.cpu_alarm_threshold
 
-  alarm_description = "This alarm monitors EC2 CPU utilization"
+  alarm_description = "EC2 CPU utilization is very high.  Please take appropriate action."
 
   dimensions = {
-    InstanceId = aws_instance.web.id
+    InstanceId = aws_instance.ec2_instance.id
   }
 
   alarm_actions = [
-    aws_sns_topic.alerts.arn
+    aws_sns_topic.ec2_cpu_alerts.arn
   ]
 
   ok_actions = [
-    aws_sns_topic.alerts.arn
+    aws_sns_topic.ec2_cpu_alerts.arn
   ]
 }
