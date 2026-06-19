8ebb978620fac5159a0ecbb4e5b1ddf7317f7a6f
arslan-ashraf <arslanash@gmail.com>
2026-06-19 14:44:07 -0400

    changes

--- 19b-custom-domain-https-app-load-balancer/outputs.tf
+++ 19b-custom-domain-https-app-load-balancer/outputs.tf
@@ -2,6 +2,6 @@ output "acm_tls_certificate" {
   value = data.aws_acm_certificate.acm_tls_certificate.arn
 }
 
-output "arslanashraf_dot_site" {
-  value = data.aws_route53_zone.arslanashraf_dot_site
+output "custom_domain" {
+  value = data.aws_route53_zone.custom_domain
 }
--- 19b-custom-domain-https-app-load-balancer/route53-custom-domain-record.tf
+++ 19b-custom-domain-https-app-load-balancer/route53-custom-domain-record.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "custom_domain" {
   private_zone = false
 }
 
-resource "aws_route53_record" "new_record" {
+resource "aws_route53_record" "custom_domain_record" {
   zone_id = data.aws_route53_zone.custom_domain.zone_id
   name    = var.custom_domain
   type    = "A"
