ae81bfe7255b3638c322d83e2c1c9cd5b46d5c3e
arslan-ashraf <arslanash@gmail.com>
2026-06-19 15:48:14 -0400

    changes

--- 27-project-cloudtrail-cloudwatch-sns-security-monitoring/cloudtrail.tf
+++ 27-project-cloudtrail-cloudwatch-sns-security-monitoring/cloudtrail.tf
@@ -5,4 +5,6 @@ resource "aws_cloudtrail" "secret_accessed_trail" {
   s3_bucket_name                = aws_s3_bucket.cloudtrail_logs_bucket.id
   include_global_service_events = false # prevents global services (e.g., IAM) from sending logs
   enable_logging                = true  # default
+
+  
 }
