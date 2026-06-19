873ac889b896c800c1402eaded793a4411a885d3
arslan-ashraf <arslanash@gmail.com>
2026-06-19 15:06:01 -0400

    changes

--- 27-cloudtrail-cloudwatch-sns-monitoring/sns-topic.tf
+++ 27-cloudtrail-cloudwatch-sns-monitoring/sns-topic.tf
@@ -2,7 +2,7 @@ resource "aws_sns_topic" "secret_accessed_alerts_topic" {
   name = "secret_accessed_alerts_topic"
 }
 
-resource "aws_sns_topic_subscription" "email" {
+resource "aws_sns_topic_subscription" "alert_by_email" {
   topic_arn = aws_sns_topic.secret_accessed_alerts_topic.arn
   protocol  = "email"
   endpoint  = var.email_for_alerts
