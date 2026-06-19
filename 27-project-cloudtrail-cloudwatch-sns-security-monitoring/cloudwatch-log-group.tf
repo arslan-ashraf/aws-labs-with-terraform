315631a179131bd406ae5ae9e2804746702bde4b
arslan-ashraf <arslanash@gmail.com>
2026-06-19 15:50:28 -0400

    changes

--- 27-project-cloudtrail-cloudwatch-sns-security-monitoring/cloudwatch-log-group.tf
+++ 27-project-cloudtrail-cloudwatch-sns-security-monitoring/cloudwatch-log-group.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "lambda_log_group"
+resource "aws_cloudwatch_log_group" "secrets_accessed_cloudwatch_log_group" {
+  name              = "secrets_accessed_cloudwatch_log_group"
   retention_in_days = 14
 }
