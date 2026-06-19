7a5e9292274916e1169f13d05539456b47f4deb6
arslan-ashraf <arslanash@gmail.com>
2026-06-19 15:22:19 -0400

    changes

--- 27-cloudtrail-cloudwatch-sns-monitoring/sns-topic.tf
+++ 27-cloudtrail-cloudwatch-sns-monitoring/sns-topic.tf
@@ -1,9 +1,33 @@
-resource "aws_sns_topic" "secret_accessed_alerts_topic" {
-  name = "secret_accessed_alerts_topic"
+resource "aws_sns_topic" "secret_accessed_alerts" {
+  name = "secret_accessed_alerts"
 }
 
 resource "aws_sns_topic_subscription" "alert_by_email" {
-  topic_arn = aws_sns_topic.secret_accessed_alerts_topic.arn
+  topic_arn = aws_sns_topic.secret_accessed_alerts.arn
   protocol  = "email"
   endpoint  = var.email_for_alerts
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = ["SNS:Publish"]
+
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+
+    resources = [
+      aws_sns_topic.secret_accessed_alerts.arn
+    ]
+  }
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.secret_accessed_alerts.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
