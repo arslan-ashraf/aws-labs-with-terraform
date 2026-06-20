92a178d3dcc23c3f3106f21fbe7dc9d82e90d23a
arslan-ashraf <arslanash@gmail.com>
2026-06-20 18:05:44 -0400

    changes

--- 27-sns-triggers-lambda-ec2-cpu-spike/sns-topic.tf
+++ 27-sns-triggers-lambda-ec2-cpu-spike/sns-topic.tf
@@ -1,9 +1,9 @@
-resource "aws_sns_topic" "ec2_cpu_alerts_sns_topic" {
-  name = "ec2_cpu_alerts_sns_topic"
+resource "aws_sns_topic" "trigger_lambda_sns_topic" {
+  name = "trigger_lambda_sns_topic"
 }
 
 resource "aws_sns_topic_subscription" "alert_by_email" {
-  topic_arn = aws_sns_topic.ec2_cpu_alerts_sns_topic.arn
+  topic_arn = aws_sns_topic.trigger_lambda_sns_topic.arn
   protocol  = "email"
-  endpoint  = var.email_for_alerts
+  endpoint  = 
 }
