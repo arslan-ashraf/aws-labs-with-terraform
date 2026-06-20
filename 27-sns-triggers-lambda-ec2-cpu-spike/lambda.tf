07eb4c6c0e1351f5452c2217161b10846842d746
arslan-ashraf <arslanash@gmail.com>
2026-06-20 18:15:56 -0400

    changes

--- 27-sns-triggers-lambda-ec2-cpu-spike/lambda.tf
+++ 27-sns-triggers-lambda-ec2-cpu-spike/lambda.tf
@@ -1,15 +1,26 @@
-# grant SNS permission to invoke your Lambda function
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "./lambda_function_code.mjs"
+  output_path = "lambda_function_code.zip"
+}
+
+locals  {
+  lambda_function_name = "cloudwatch-alarm-response-lambda-handler"
+}
 
 resource "aws_lambda_function" "target_lambda" {
-  filename      = "lambda_function_payload.zip"
-  function_name = "alarm-response-handler"
+  filename      = "lambda_function_code.zip"
+  function_name = "cloudwatch-alarm-response-lambda-handler"
   role          = aws_iam_role.lambda_execution_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
+  handler       = "lambda_function_code.handler"
+  runtime       = "nodejs24.x"
+
+  memory_size   = 128       # in megabytes
+  timeout       = 5         # in seconds
 }
 
 
-# Lambda resource based policy
+# Lambda resource based policy to allow SNS to invoke the Lambda function
 resource "aws_lambda_permission" "allow_sns_invoke_lambda" {
   statement_id  = "Allow Lambda execution from SNS"
   action        = "lambda:InvokeFunction"
