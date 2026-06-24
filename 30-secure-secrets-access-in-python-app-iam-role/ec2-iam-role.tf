cfc8f1c185ded3da95c5f387061f95bf7ac8706b
arslan-ashraf <arslanash@gmail.com>
2026-06-24 12:19:26 -0400

    changes

--- 30-secure-secrets-access-in-python-app-iam-role/ec2-iam-role.tf
+++ 30-secure-secrets-access-in-python-app-iam-role/ec2-iam-role.tf
@@ -29,19 +29,19 @@ data "aws_iam_policy_document" "ec2_s3_access_permissions" {
   }
 }
 
-resource "aws_iam_policy" "secrets_read_policy" {
-  name   = "ec2-secrets-manager-read-policy"
+resource "aws_iam_policy" "ec2_s3_read_policy" {
+  name   = "ec2_s3_read_policy"
   policy = data.aws_iam_policy_document.ec2_s3_access_permissions.json
 }
 
 
-resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+resource "aws_iam_role_policy_attachment" "attach_role_and_policy" {
   role       = aws_iam_role.ec2_secrets_access_role.name
   policy_arn = aws_iam_policy.secrets_read_policy.arn
 }
 
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-secrets-manager-instance-profile"
+  name = "ec2_read_s3_profile"
   role = aws_iam_role.ec2_secrets_access_role.name
 }
