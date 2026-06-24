7dfef1ea1b0fef4349c8395a55aa839302248a5c
arslan-ashraf <arslanash@gmail.com>
2026-06-24 12:25:48 -0400

    changes

--- 30-secure-secrets-access-in-python-app-iam-role/basic-python-app/setup.sh
+++ 30-secure-secrets-access-in-python-app-iam-role/basic-python-app/setup.sh
@@ -11,3 +11,12 @@
 
 set -euo pipefail
 
+sudo apt update && sudo apt install python3-pip python3-venv python3-full -y
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip3 install -r requirements.txt
+
+python3 app.py
