22ae98dee14b54ac85e5bdc767dd877d1f13b3aa
arslan-ashraf <arslanash@gmail.com>
2026-06-26 14:13:12 -0400

    changes

--- 23-cloudfront-s3-presigned-urls/cloudfront-public-key.tf
+++ 23-cloudfront-s3-presigned-urls/cloudfront-public-key.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudfront_public_key" "cloudfront_public_key" {
-  comment = "Optional"
-  name = "Optional"
-  name_prefix = "Optional"
+  comment     = "Public key for securing access to S3"
+  name        = "cloudfront_public_key"
+  encoded_key = file("public_key_for_cloudfront.pem")
 }
 
 resource "aws_cloudfront_key_group" "cloudfront_public_key_group" {
-  
+
 }
