fcadaee8929abb1c415a617ebd11f92369e8ea3b
arslan-ashraf <arslanash@gmail.com>
2026-06-26 18:57:56 -0400

    changes

--- 31-basic-api-gateway-to-lambda/api-gateway.tf
+++ 31-basic-api-gateway-to-lambda/api-gateway.tf
@@ -1,18 +1,18 @@
 # REST API gateway
-resource "aws_api_gateway_rest_api" "this" {
-  name = "example-api"
+resource "aws_api_gateway_rest_api" "rest_api_gateway" {
+  name = "rest_api_gateway"
 }
 
 
-# # HTTP API gateway
-# resource "aws_apigatewayv2_api" "example" {
-#   name          = "example-http-api"
+# HTTP API gateway
+# resource "aws_apigatewayv2_api" "http_api_gateway" {
+#   name          = "http_api_gateway"
 #   protocol_type = "HTTP"
 # }
 
 # # Websocket API gateway
-# resource "aws_apigatewayv2_api" "example" {
-#   name                       = "example-websocket-api"
+# resource "aws_apigatewayv2_api" "websocket_api_gateway" {
+#   name                       = "websocket_api_gateway"
 #   protocol_type              = "WEBSOCKET"
 #   route_selection_expression = "$request.body.action"
 # }
