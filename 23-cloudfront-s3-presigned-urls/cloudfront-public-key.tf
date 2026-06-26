888800615c0f1cab3092aa7471d5881a1a6ebffb
arslan-ashraf <arslanash@gmail.com>
2026-06-26 14:11:51 -0400

    changes

---
+++ 23-cloudfront-s3-presigned-urls/cloudfront-public-key.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudfront_public_key" "cloudfront_public_key" {
+  comment = "Optional"
+  name = "Optional"
+  name_prefix = "Optional"
+}
+
+resource "aws_cloudfront_key_group" "cloudfront_public_key_group" {
+  
+}
