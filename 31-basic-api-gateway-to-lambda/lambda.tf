619f5e2fa37a9e2bb415e1acadeb7a5af04595b9
arslan-ashraf <arslanash@gmail.com>
2026-06-26 17:56:47 -0400

    changes

--- 31-basic-api-gateway-to-lambda/lambda.tf
+++ 31-basic-api-gateway-to-lambda/lambda.tf
@@ -5,31 +5,9 @@ data "archive_file" "lambda_zip" {
   output_path = "lambda_function_code.zip"
 }
 
-# role that a user will temporarily have to be able to use the 
-# presigned URL we import the arn of this role through the Lambda 
-# function's environment variables
-data "aws_iam_policy_document" "generate_presigned_url_policy_document" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "presigned_url_user_role" {
-  name               = "presigned_url_user_role"
-  assume_role_policy = data.aws_iam_policy_document.generate_presigned_url_policy_document.json
-}
-
-# Lambda function to generate the presigned URL
-resource "aws_lambda_function" "presigned_url_generator_lambda" {
+resource "aws_lambda_function" "get_user_data_dynamoDB_lambda" {
   filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "generate-presigned-s3-url"
+  function_name    = "get-user-data-from-dynamoDB-table"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function_code.handler"
   runtime          = "nodejs24.x"
