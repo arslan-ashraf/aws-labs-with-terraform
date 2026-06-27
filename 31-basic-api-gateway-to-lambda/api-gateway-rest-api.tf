26f5a522f964ff9d754cd304ad8cdbb49ba5a56f
arslan-ashraf <arslanash@gmail.com>
2026-06-26 20:58:08 -0400

    changes

--- 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
+++ 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
@@ -25,7 +25,8 @@ resource "aws_api_gateway_integration" "example" {
   rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
   resource_id = aws_api_gateway_resource.users_path.id
   http_method = aws_api_gateway_method.GET_users.http_method
-  type        = "MOCK"
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.get_user_data_dynamoDB_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "example" {
