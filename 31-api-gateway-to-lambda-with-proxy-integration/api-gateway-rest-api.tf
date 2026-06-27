a46df731285023f2be904b431e47bdea5d19aacd
arslan-ashraf <arslanash@gmail.com>
2026-06-27 09:32:38 -0400

    changes

--- 31-api-gateway-to-lambda-with-proxy-integration/api-gateway-rest-api.tf
+++ 31-api-gateway-to-lambda-with-proxy-integration/api-gateway-rest-api.tf
@@ -33,7 +33,7 @@ resource "aws_api_gateway_integration" "integrate_GET_users_lambda" {
   rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
   resource_id = aws_api_gateway_resource.users_path.id
   http_method = aws_api_gateway_method.GET_users.http_method
-  type        = "AWS_PROXY"   # pass full request to Lambda
+  type        = "AWS_PROXY"         # pass full request to Lambda
   integration_http_method = "POST"  # for Lambda, always "POST"
   uri         = aws_lambda_function.get_user_data_dynamoDB_lambda.invoke_arn
 }
