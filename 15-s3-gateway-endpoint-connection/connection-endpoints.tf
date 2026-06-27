4bfade20eb6a07375b835acc44fea064e8ec6130
arslan-ashraf <arslanash@gmail.com>
2026-06-26 20:40:54 -0400

    changes

--- 15-s3-gateway-endpoint-connection/connection-endpoints.tf
+++ 15-s3-gateway-endpoint-connection/connection-endpoints.tf
@@ -26,20 +26,29 @@ data "aws_vpc_endpoint_service" "s3_endpoint" {
 data "aws_iam_policy_document" "s3_endpoint_permissions" {
   statement {
     effect    = "Allow"
+
+    # principals {
+    #   type        = "*"
+    #   identifiers = ["*"]
+    # }
+
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "AWS"
+      identifiers = [aws_iam_role.ec2_sqs_access_role.arn]
     }
-    resources = [
-      aws_s3_bucket.example_bucket.arn, 
-      "${aws_s3_bucket.example_bucket.arn}/*"
-    ]
+    
     actions = [
       "s3:ListBucket",
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject"
     ]
+
+    resources = [
+      aws_s3_bucket.example_bucket.arn, 
+      "${aws_s3_bucket.example_bucket.arn}/*"
+    ]
+    
   }
 }
 
