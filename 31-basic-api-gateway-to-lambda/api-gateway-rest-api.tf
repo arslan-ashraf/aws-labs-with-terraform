5d737740e4a9866215723f5fa0c5a0867028c81f
arslan-ashraf <arslanash@gmail.com>
2026-06-26 20:52:03 -0400

    changes

--- 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
+++ 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
@@ -8,10 +8,10 @@ resource "aws_api_gateway_rest_api" "rest_api_gateway" {
   }
 }
 
-resource "aws_api_gateway_resource" "resource_path" {
+resource "aws_api_gateway_resource" "users_path" {
   rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
   parent_id   = aws_api_gateway_rest_api.rest_api_gateway.root_resource_id
-  path_part   = "example"
+  path_part   = "users"
 }
 
 resource "aws_api_gateway_method" "example" {
