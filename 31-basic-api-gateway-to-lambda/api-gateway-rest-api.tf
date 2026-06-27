b2d1964548701154f20e3175fb28dd7f585ff8d7
arslan-ashraf <arslanash@gmail.com>
2026-06-26 20:54:15 -0400

    changes

--- 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
+++ 31-basic-api-gateway-to-lambda/api-gateway-rest-api.tf
@@ -22,9 +22,9 @@ resource "aws_api_gateway_method" "GET_users" {
 }
 
 resource "aws_api_gateway_integration" "example" {
-  http_method = aws_api_gateway_method.example.http_method
-  resource_id = aws_api_gateway_resource.example.id
-  rest_api_id = aws_api_gateway_rest_api.example.id
+  rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
+  resource_id = aws_api_gateway_resource.users_path.id
+  http_method = aws_api_gateway_method.GET_users.http_method
   type        = "MOCK"
 }
 
