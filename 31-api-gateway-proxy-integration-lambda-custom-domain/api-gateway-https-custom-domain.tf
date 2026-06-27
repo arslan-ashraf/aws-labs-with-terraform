6da2b0d272859080248c9d330e3864f1ab81851b
arslan-ashraf <arslanash@gmail.com>
2026-06-27 13:44:28 -0400

    changes

--- 31-api-gateway-proxy-integration-lambda-custom-domain/api-gateway-https-custom-domain.tf
+++ 31-api-gateway-proxy-integration-lambda-custom-domain/api-gateway-https-custom-domain.tf
@@ -15,4 +15,5 @@ resource "aws_api_gateway_base_path_mapping" "custom_domain_gateway" {
   api_id      = aws_api_gateway_rest_api.rest_api_gateway.id
   stage_name  = aws_api_gateway_stage.production_stage.stage_name
   domain_name = aws_api_gateway_domain_name.custom_domain.domain_name
+  # base_path   = "api"  # yields api.<custom_domain>
 }
--- 31-api-gateway-proxy-integration-lambda-custom-domain/route53-custom-domain-record.tf
+++ 31-api-gateway-proxy-integration-lambda-custom-domain/route53-custom-domain-record.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "custom_domain_record" {
   alias {
     name                   = aws_api_gateway_domain_name.custom_domain.regional_domain_name
     zone_id                = aws_api_gateway_domain_name.custom_domain.regional_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = true
   }
   
 }
