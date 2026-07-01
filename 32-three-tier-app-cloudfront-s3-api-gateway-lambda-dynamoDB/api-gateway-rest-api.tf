a464b3a2a42c4c17a68c0f16ed5b0c5aaab1d013
arslan-ashraf <arslanash@gmail.com>
2026-07-01 13:00:21 -0400

    changes

--- 32-three-tier-app-cloudfront-s3-api-gateway-lambda-dynamoDB/api-gateway-rest-api.tf
+++ 32-three-tier-app-cloudfront-s3-api-gateway-lambda-dynamoDB/api-gateway-rest-api.tf
@@ -93,17 +93,3 @@ resource "aws_api_gateway_stage" "production_stage" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api_gateway.id
   stage_name    = "production"
 }
-
-
-# HTTP API gateway
-# resource "aws_apigatewayv2_api" "http_api_gateway" {
-#   name          = "http_api_gateway"
-#   protocol_type = "HTTP"
-# }
-
-# # Websocket API gateway
-# resource "aws_apigatewayv2_api" "websocket_api_gateway" {
-#   name                       = "websocket_api_gateway"
-#   protocol_type              = "WEBSOCKET"
-#   route_selection_expression = "$request.body.action"
-# }
