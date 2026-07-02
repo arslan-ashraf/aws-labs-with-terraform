8d0515632f9b57d5decf422847a50e4c834a1a08
arslan-ashraf <arslanash@gmail.com>
2026-07-02 16:49:41 -0400

    changes

--- 31-api-gateway-proxy-lambda-dynamoDB-custom-domain/api-gateway-rest-api.tf
+++ 31-api-gateway-proxy-lambda-dynamoDB-custom-domain/api-gateway-rest-api.tf
@@ -19,8 +19,8 @@ resource "aws_api_gateway_resource" "users_path" {
 resource "aws_api_gateway_method" "GET_users" {
   authorization = "NONE"
   http_method   = "GET"
-  resource_id   = aws_api_gateway_resource.users_path.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api_gateway.id
+  resource_id   = aws_api_gateway_resource.users_path.id
 }
 
 
