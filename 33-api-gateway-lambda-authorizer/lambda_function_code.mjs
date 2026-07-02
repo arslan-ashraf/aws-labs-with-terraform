7db2e38108066c868474e98b9d97dd0f471f2ecc
arslan-ashraf <arslanash@gmail.com>
2026-07-02 18:00:00 -0400

    changes

--- 33-api-gateway-lambda-authorizer/lambda_function_code.mjs
+++ 33-api-gateway-lambda-authorizer/lambda_function_code.mjs
@@ -1,23 +1,13 @@
 async function handler(event) {
 
-    const query_string_params = event.queryStringParameters
-
-    if (query_string_params != null){
-
-        
-        
-    } else {
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ 
-                message: "queryStringParameters is null", 
-                invocation: "Success",
-                event: event
-            }),
-            headers: {'Content-Type': 'application/json'}
-        };
-
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ 
+            lambda_invocation: "Success",
+            message: "Backend Lambda successfully invoked", 
+            event: event
+        }),
+        headers: {'Content-Type': 'application/json'}
     }
 
 }
