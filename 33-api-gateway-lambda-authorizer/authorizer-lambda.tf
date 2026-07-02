6449c41e658b26783203dea2f019d0f936f8fb22
arslan-ashraf <arslanash@gmail.com>
2026-07-02 18:34:50 -0400

    changes

--- 33-api-gateway-lambda-authorizer/authorizer-lambda.tf
+++ 33-api-gateway-lambda-authorizer/authorizer-lambda.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "authorizer_lambda" {
   filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "backend_lambda"
+  function_name    = "authorizer_lambda"
   role             = aws_iam_role.authorizer_lambda_role.arn
   handler          = "token_based_lambda_authorizer_code.handler"
   runtime          = "nodejs24.x"
@@ -22,6 +22,6 @@ resource "aws_lambda_permission" "api_gateway_invoke_lambda_permission" {
   statement_id  = "allow_api_gateway_to_invoke_authorizer_lambda"
   principal     = "apigateway.amazonaws.com"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.backend_lambda.function_name
+  function_name = aws_lambda_function.authorizer_lambda.function_name
   source_arn    = "${aws_api_gateway_rest_api.rest_api_gateway.execution_arn}/*/GET/users"
 }
