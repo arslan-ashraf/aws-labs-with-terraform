85fbe41b532f3516bee9d1efd85a2e30d2e5c975
arslan-ashraf <arslanash@gmail.com>
2026-07-02 16:51:39 -0400

    changes

--- 33-api-gateway-lambda-authorizer/backend-lambda-iam-role.tf
+++ 33-api-gateway-lambda-authorizer/backend-lambda-iam-role.tf
@@ -1,7 +1,3 @@
-#################################################################
-# EVERYTHING HERE IS NOT NEEDED IN THIS LAB, ITS FOR A LATER LAB
-#################################################################
-
 # define user/role based policy
 data "aws_iam_policy_document" "lambda_trust_policy_document" {
   statement {
@@ -16,51 +12,51 @@ data "aws_iam_policy_document" "lambda_trust_policy_document" {
   }
 }
 
-resource "aws_iam_role" "lambda_dynamoDB_role" {
-  name               = "lambda_dynamoDB_role"
+resource "aws_iam_role" "backend_lambda_role" {
+  name               = "backend_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy_document.json
 }
 
 
 # IAM Policy to allow Lambda to issue PutObject/GetObject actions
-data "aws_iam_policy_document" "lambda_dynamoDB_CRUD_permissions" {
+# data "aws_iam_policy_document" "lambda_dynamoDB_CRUD_permissions" {
 
-  statement {
-    effect    = "Allow"
+#   statement {
+#     effect    = "Allow"
 
-    actions   = [
-      "dynamodb:GetItem",
-      "dynamodb:DeleteItem",
-      "dynamodb:PutItem",
-      "dynamodb:Scan",
-      "dynamodb:Query",
-      "dynamodb:UpdateItem",
-      "dynamodb:BatchWriteItem",
-      "dynamodb:BatchGetItem",
-      "dynamodb:DescribeTable",
-    ]
+#     actions   = [
+#       "dynamodb:GetItem",
+#       "dynamodb:DeleteItem",
+#       "dynamodb:PutItem",
+#       "dynamodb:Scan",
+#       "dynamodb:Query",
+#       "dynamodb:UpdateItem",
+#       "dynamodb:BatchWriteItem",
+#       "dynamodb:BatchGetItem",
+#       "dynamodb:DescribeTable",
+#     ]
 
-    resources = [
-      "${aws_dynamodb_table.users_table.arn}",
-      "${aws_dynamodb_table.users_table.arn}/*"
-    ]
-  }
+#     resources = [
+#       "${aws_dynamodb_table.users_table.arn}",
+#       "${aws_dynamodb_table.users_table.arn}/*"
+#     ]
+#   }
 
-  statement {
-    effect = "Allow"
-    actions = ["dynamodb:ListTables"]
-    resources = ["*"]
-  }
+#   statement {
+#     effect = "Allow"
+#     actions = ["dynamodb:ListTables"]
+#     resources = ["*"]
+#   }
 
-}
+# }
 
-resource "aws_iam_policy" "lambda_dynamoDB_CRUD_policy" {
-  name   = "lambda_dynamoDB_CRUD_policy"
-  policy = data.aws_iam_policy_document.lambda_dynamoDB_CRUD_permissions.json
-}
+# resource "aws_iam_policy" "lambda_dynamoDB_CRUD_policy" {
+#   name   = "lambda_dynamoDB_CRUD_policy"
+#   policy = data.aws_iam_policy_document.lambda_dynamoDB_CRUD_permissions.json
+# }
 
 
-resource "aws_iam_role_policy_attachment" "lambda_s3_role_policy_attachment" {
-  role       = aws_iam_role.lambda_dynamoDB_role.name
-  policy_arn = aws_iam_policy.lambda_dynamoDB_CRUD_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "lambda_s3_role_policy_attachment" {
+#   role       = aws_iam_role.lambda_dynamoDB_role.name
+#   policy_arn = aws_iam_policy.lambda_dynamoDB_CRUD_policy.arn
+# }
