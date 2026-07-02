6719d5c5f85e682a7747af21135491c1bdf95b1c
arslan-ashraf <arslanash@gmail.com>
2026-07-02 18:36:18 -0400

    changes

---
+++ 33-api-gateway-lambda-authorizer/authorizer-lambda-iam-role.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "lambda_trust_policy_document" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "authorizer_lambda_role" {
+  name               = "authorizer_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy_document.json
+}
