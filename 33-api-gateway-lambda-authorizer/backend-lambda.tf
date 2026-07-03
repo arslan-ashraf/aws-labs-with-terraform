39f11e5ce32a80cc46b7bce2a23e93aa8ec26410
arslan-ashraf <arslanash@gmail.com>
2026-07-03 14:08:58 -0400

    changes

--- 33-api-gateway-lambda-authorizer/backend-lambda.tf
+++ 33-api-gateway-lambda-authorizer/backend-lambda.tf
@@ -1,24 +1,24 @@
 # zip compilation for the Lambda source code
-data "archive_file" "lambda_zip" {
+data "archive_file" "backend_lambda_zip" {
   type        = "zip"
   source_file = "./backend_lambda_function_code.mjs"
   output_path = "backend_lambda_function_code.zip"
 }
 
 resource "aws_lambda_function" "backend_lambda" {
-  filename         = data.archive_file.lambda_zip.output_path
+  filename         = data.archive_file.backend_lambda_zip.output_path
   function_name    = "backend_lambda"
   role             = aws_iam_role.backend_lambda_role.arn
   handler          = "backend_lambda_function_code.handler"
   runtime          = "nodejs24.x"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  source_code_hash = data.archive_file.backend_lambda_zip.output_base64sha256
 
   memory_size = 128 # in megabytes
   timeout     = 5   # in seconds
 
 }
 
-resource "aws_lambda_permission" "api_gateway_invoke_lambda_permission" {
+resource "aws_lambda_permission" "api_gateway_invoke_backend_lambda_permission" {
   statement_id  = "allow_api_gateway_to_invoke_backend_lambda"
   principal     = "apigateway.amazonaws.com"
   action        = "lambda:InvokeFunction"
