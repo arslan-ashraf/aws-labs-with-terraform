03f9b4fb2b19846a12a3ae23bb09a684fd0b7a4f
arslan-ashraf <arslanash@gmail.com>
2026-07-04 23:31:13 -0400

    changes

--- 35-ec2-auto-scaling/ec2-launch-template.tf
+++ 35-ec2-auto-scaling/ec2-launch-template.tf
@@ -5,7 +5,7 @@ resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
   # iam_instance_profile = ""
   image_id = "Required"
   instance_initiated_shutdown_behavior = "Required"
-  instance_market_options = "Required"
+
   instance_type = "Required"
   kernel_id = "Required"
   key_name = "Required"
@@ -16,6 +16,5 @@ resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
   ram_disk_id = "Required"
   security_group_names = "Required"
   vpc_security_group_ids = "Required"
-  tag_specifications = "Required"
-  tags = "Optional"
+
 }
