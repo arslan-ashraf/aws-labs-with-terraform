abd247fadbe6db610d0aa977b581490e1707c90b
arslan-ashraf <arslanash@gmail.com>
2026-07-08 14:29:07 -0400

    changes

--- 35-ec2-auto-scaling/auto-scaling-policy.tf
+++ 35-ec2-auto-scaling/auto-scaling-policy.tf
@@ -4,3 +4,16 @@
 # desired capacity, the aws_auto_scaling_policy determines when and
 # how to add or remove instances within those limits based on 
 # real-time metrics or schedules
+
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "cpu-target-tracking-policy"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0 # Target 50% CPU utilization
+  }
+}
