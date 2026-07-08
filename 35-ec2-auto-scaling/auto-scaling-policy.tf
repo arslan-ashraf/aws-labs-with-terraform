a622123721f7bfb08b7584e442416f9ca1cd179f
arslan-ashraf <arslanash@gmail.com>
2026-07-08 15:47:50 -0400

    changes

--- 35-ec2-auto-scaling/auto-scaling-policy.tf
+++ 35-ec2-auto-scaling/auto-scaling-policy.tf
@@ -6,7 +6,7 @@
 # real-time metrics or schedules
 
 resource "aws_autoscaling_policy" "ASG_cpu_target_tracking_policy" {
-  name                   = "cpu-target-tracking-policy"
+  name                   = "ASG_cpu_target_tracking_policy"
   autoscaling_group_name = aws_autoscaling_group.ec2_autoscaling_group.name
   policy_type            = "TargetTrackingScaling"
 
@@ -14,10 +14,65 @@ resource "aws_autoscaling_policy" "ASG_cpu_target_tracking_policy" {
     # trigger when average CPU utilization of the desired capacity
     # reaches 50%
     target_value = 50.0
-    
+
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
 
+  }
+}
+
+
+resource "aws_autoscaling_policy" "ASG_cpu_target_tracking_policy" {
+  name                   = "ASG_cpu_target_tracking_policy"
+  autoscaling_group_name = aws_autoscaling_group.ec2_autoscaling_group.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    
+    target_value = 100
+
+    customized_metric_specification {
+      metrics {
+        label = "Get the queue size (the number of messages waiting to be processed)"
+        id    = "m1"
+        metric_stat {
+          metric {
+            namespace   = "AWS/SQS"
+            metric_name = "ApproximateNumberOfMessagesVisible"
+            dimensions {
+              name  = "QueueName"
+              value = "my-queue"
+            }
+          }
+          stat   = "Sum"
+          period = 10
+        }
+        return_data = false
+      }
+      metrics {
+        label = "Get the group size (the number of InService instances)"
+        id    = "m2"
+        metric_stat {
+          metric {
+            namespace   = "AWS/AutoScaling"
+            metric_name = "GroupInServiceInstances"
+            dimensions {
+              name  = "AutoScalingGroupName"
+              value = "my-asg"
+            }
+          }
+          stat   = "Average"
+          period = 10
+        }
+        return_data = false
+      }
+      metrics {
+        label       = "Calculate the backlog per instance"
+        id          = "e1"
+        expression  = "m1 / m2"
+        return_data = true
+      }
+    }
   }
 }
--- 35-ec2-auto-scaling/user_data.sh
+++ 35-ec2-auto-scaling/user_data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
+# install Docker
+# add Docker's official GPG key:
 sudo apt update -y
 sudo apt install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
