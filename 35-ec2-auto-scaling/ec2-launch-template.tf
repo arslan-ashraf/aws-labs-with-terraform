f77c864ac935a89f068bfa89b5f7f873dd7d1e1e
arslan-ashraf <arslanash@gmail.com>
2026-07-08 11:25:51 -0400

    changes

--- 35-ec2-auto-scaling/ec2-launch-template.tf
+++ 35-ec2-auto-scaling/ec2-launch-template.tf
@@ -1,12 +1,5 @@
 resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
   name = "ec2_auto_scaling_launch_template"
-  
-
-  # iam_instance_profile = ""
-
-  # block_device_mappings = {}  # EBS volumes to attach
-  # instance_initiated_shutdown_behavior = "stop"  # default "stop"
-  # monitoring = "Required"
 
   image_id = "ami-0ec10929233384c7f"  # ubuntu ami
   instance_type = "t2.nano"
@@ -17,6 +10,12 @@ resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
   }
 
   vpc_security_group_ids = [""]
+
+  # iam_instance_profile = ""
+
+  # block_device_mappings = {}  # EBS volumes to attach
+  # instance_initiated_shutdown_behavior = "stop"  # default "stop"
+  # monitoring = ""
 
 }
 
