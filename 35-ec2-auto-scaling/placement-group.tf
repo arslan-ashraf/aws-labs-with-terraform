a2483aaa73de75e522ba62654823d37a5299bd32
arslan-ashraf <arslanash@gmail.com>
2026-07-08 12:31:27 -0400

    changes

--- 35-ec2-auto-scaling/placement-group.tf
+++ 35-ec2-auto-scaling/placement-group.tf
@@ -1,4 +1,8 @@
 resource "aws_placement_group" "app_placement" {
-  name     = "app-cluster-pg"
+  name     = "ec2_placement_group"
+
+  # strategy determines he placement strategy for the group which
+  # must be one of the following:
+  # cluster - Packs instances close together inside a single Availability Zone. Best for low-latency network performance.spread: Places instances on distinct physical hardware racks to reduce correlated failures. Max 7 instances per AZ.partition: Divides the group into logical partitions; instances in one partition do not share hardware with other partitions.
   strategy = "cluster"
 }
