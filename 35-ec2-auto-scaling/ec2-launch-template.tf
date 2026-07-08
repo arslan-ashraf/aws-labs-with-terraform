1ff3834d6a16ed99cad7f1d74bd8312768a7fe04
arslan-ashraf <arslanash@gmail.com>
2026-07-08 11:23:24 -0400

    changes

--- 35-ec2-auto-scaling/ec2-launch-template.tf
+++ 35-ec2-auto-scaling/ec2-launch-template.tf
@@ -6,13 +6,12 @@ resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
 
   # block_device_mappings = {}  # EBS volumes to attach
   # instance_initiated_shutdown_behavior = "stop"  # default "stop"
+  # monitoring = "Required"
 
   image_id = "ami-0ec10929233384c7f"  # ubuntu ami
   instance_type = "t2.nano"
   key_name      = "key-for-ec2-connection"
 
-  monitoring = "Required"
-  network_interfaces = "Required"
   placement = "Required"
   ram_disk_id = "Required"
   security_group_names = "Required"
