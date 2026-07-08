dff0f49b5d587bdff6b4462a1c6b7687b9f20168
arslan-ashraf <arslanash@gmail.com>
2026-07-08 13:15:06 -0400

    changes

--- 35-ec2-auto-scaling/auto-scaling-group.tf
+++ 35-ec2-auto-scaling/auto-scaling-group.tf
@@ -3,7 +3,10 @@ resource "aws_autoscaling_group" "ec2_autoscaling_group" {
   desired_capacity    = 1
   max_size            = 3
   min_size            = 1
-  vpc_zone_identifier = ["subnet-12345678", "subnet-87654321"]
+  vpc_zone_identifier = [
+    aws_subnet.public_subnet_1_for_ec2.id, 
+    aws_subnet.public_subnet_2_for_ec2.id
+  ]
 
   # Attach instances to your physical hardware placement group
   placement_group = aws_placement_group.app_placement.id
