62fae61fe3e59638cdb2809ab81c640664f169d8
arslan-ashraf <arslanash@gmail.com>
2026-07-08 12:38:13 -0400

    changes

--- 35-ec2-auto-scaling/placement-group.tf
+++ 35-ec2-auto-scaling/placement-group.tf
@@ -1,8 +1,10 @@
 # aws_placement_group influences the physical placement of EC2
 # instances within AWS data centers for performance or HA needs
-resource "aws_placement_group" "ec2-autoscaling-placement-group" {
-  name     = "ec2-autoscaling-placement-group"
 
+resource "aws_placement_group" "ec2_autoscaling_placement_group" {
+  name     = "ec2_autoscaling_placement_group"
+
+  strategy = "cluster"
   # strategy determines the placement strategy for the group which
   # must be one of the following:
 
@@ -14,7 +16,7 @@ resource "aws_placement_group" "ec2-autoscaling-placement-group" {
 
   # partition - divides the group into logical partitions, instances in
   # one partition do not share hardware with other partitions
-  strategy = "cluster"
+  
 
   # partition_count is the number of partitions to create 
   # (valid values are 1 to 7, defaults to 2), only valid if 
