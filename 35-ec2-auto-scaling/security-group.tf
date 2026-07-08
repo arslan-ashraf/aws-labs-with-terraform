cedc0a0031081e40a5dfb583835ab4fa0fba1d7e
arslan-ashraf <arslanash@gmail.com>
2026-07-08 13:12:53 -0400

    changes

--- 35-ec2-auto-scaling/security-group.tf
+++ 35-ec2-auto-scaling/security-group.tf
@@ -1,16 +1,67 @@
-resource "aws_security_group" "security_group_public_traffic" {
-  name   = "security-group-for-public-traffic"
+#########################################################################
+##################### EC2 SECURITY GROUP & RULES ########################
+#########################################################################
+
+resource "aws_security_group" "security_group_for_ec2_instance" {
+  name   = "security_group_for_ec2_instance"
   vpc_id = aws_vpc.example_vpc.id
-  tags   = { Name = "sg-for-public-traffic" }
+  tags   = { Name = "security_group_for_ec2_instance" }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
-  security_group_id = aws_security_group.security_group_public_traffic.id
-  cidr_ipv4         = "0.0.0.0/0" # where is the traffic coming from
+# allow the load balancer make http requests the EC2 instance
+resource "aws_vpc_security_group_ingress_rule" "ingress_from_load_balancer_rule" {
+  security_group_id = aws_security_group.security_group_for_ec2_instance.id
 
-  # to allow ingress traffic for ssh
-  from_port = 22
-  to_port   = 22
+  # where is the traffic coming from
+  referenced_security_group_id = aws_security_group.security_group_for_application_load_balancer.id
+
+  from_port = 80
+  to_port   = 80
+
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_internet_rule" {
+  security_group_id = aws_security_group.security_group_for_ec2_instance.id
+
+  # where is the traffic going
+  cidr_ipv4 = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+}
+
+
+########################################################################
+################# LOAD BALANCER SECURITY GROUP & RULES #################
+########################################################################
+
+
+resource "aws_security_group" "security_group_for_application_load_balancer" {
+  name   = "security_group_for_application_load_balancer"
+  vpc_id = aws_vpc.example_vpc.id
+  tags   = { Name = "security_group_for_application_load_balancer" }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_all_public_traffic_rule" {
+  security_group_id = aws_security_group.security_group_for_application_load_balancer.id
+
+  # where is the traffic coming from
+  cidr_ipv4 = "0.0.0.0/0"
+
+  from_port = 80
+  to_port   = 80
+
+  ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_all_public_traffic_to_ec2_rule" {
+  security_group_id = aws_security_group.security_group_for_application_load_balancer.id
+
+  # where is the traffic going
+  referenced_security_group_id = aws_security_group.security_group_for_ec2_instance.id
+
+  from_port = 80
+  to_port   = 80
 
   ip_protocol = "tcp"
 }
