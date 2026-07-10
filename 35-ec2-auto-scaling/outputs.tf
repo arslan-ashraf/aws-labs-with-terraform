20afe2b90dd0d63be3c579e5455237d8976a9ed8
arslan-ashraf <arslanash@gmail.com>
2026-07-10 14:44:25 -0400

    changes

--- 35-ec2-auto-scaling/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "custom_domain" {
-  value = data.aws_route53_zone.custom_domain
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.application_load_balancer.dns_name
-  description = "The public URL of the application load balancer"
-}
