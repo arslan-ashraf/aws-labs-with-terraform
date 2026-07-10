28f76fa3be6d23952bb0912023e5ad2244da11d7
arslan-ashraf <arslanash@gmail.com>
2026-07-10 14:46:24 -0400

    changes

--- 35-ec2-auto-scaling/application-load-balancer.tf
+++ 35-ec2-auto-scaling/application-load-balancer.tf
@@ -62,7 +62,7 @@ resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = data.aws_acm_certificate.acm_tls_certificate.arn
 
   default_action {
