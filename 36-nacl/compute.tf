a65a87147f523d089c9af9f6c55ce4304be32822
arslan-ashraf <arslanash@gmail.com>
2026-07-11 20:20:00 -0400

    changes

--- 36-nacl/compute.tf
+++ 36-nacl/compute.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type               = "t2.nano"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public_subnet_in_example_vpc.id
-  key_name                    = "key-for-ec2-connection"
+  key_name                    = aws.aws_key_pair.public_private_key_pair.key_name
 
   vpc_security_group_ids = [
     aws_security_group.security_group_public_traffic.id
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_instance" {
 
 }
 
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "public_private_key_pair" {
   key_name   = "key-for-ec2-connection"
   public_key = file("~/.ssh/key-for-ec2-connection.pub")
 }
