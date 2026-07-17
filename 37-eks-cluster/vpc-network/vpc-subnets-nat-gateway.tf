3a0cef9b3a020c3c20821b3c41cac41f8407772b
arslan-ashraf <arslanash@gmail.com>
2026-07-17 15:48:16 -0400

    changes

--- 37-eks-cluster/vpc-network/vpc-subnets-nat-gateway.tf
+++ 37-eks-cluster/vpc-network/vpc-subnets-nat-gateway.tf
@@ -7,7 +7,7 @@ module "vpc_and_subnets_module" {
   }
 
   subnet_config = {
-    # subnets 
+    # subnets a and b are private for hosting EKS worker nodes
     subnet_a = {
       cidr_block = "10.0.1.0/24"
       public     = false
@@ -18,6 +18,9 @@ module "vpc_and_subnets_module" {
       public     = false
       AZ         = "us-east-1b"
     }
+
+    # subnet c is for hosting the NAT Gateway so worker nodes can
+    # pull Docker images, install Helm charts, etc.
     subnet_c = {
       cidr_block = "10.0.3.0/24"
       public     = true
