ea5205b91777c71c39b39ff3be44b79ffd2201e9
arslan-ashraf <arslanash@gmail.com>
2026-07-17 15:33:10 -0400

    changes

--- 37-eks-cluster/vpc-network/module/main.tf
+++ 37-eks-cluster/vpc-network/module/main.tf
@@ -3,9 +3,9 @@ data "aws_availability_zones" "AZs" {
 }
 
 locals {
-  azs             = slice(data.aws_availability_zones.AZs.names, 0, 3)
-  public_subnets  = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
-  private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
+  AZs             = slice(data.aws_availability_zones.AZs.names, 0, 3)
+  public_subnets  = [for k, AZ in local.AZs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
+  private_subnets = [for k, AZ in local.AZs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
 }
 
 resource "aws_vpc" "main" {
@@ -18,13 +18,13 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main.id
-  tags = merge(var.tags, { Name = "${var.environment_name}-igw" })
+  tags = merge(var.tags, { Name = "${var.environment_name}-internet-gateway" })
 }
 
 resource "aws_subnet" "public" {
-  for_each = { for idx, az in local.azs : az => local.public_subnets[idx] }
+  for_each = { for idx, AZ in local.AZs : AZ => local.public_subnets[idx] }
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.value
   availability_zone       = each.key
@@ -36,7 +36,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  for_each = { for idx, az in local.azs : az => local.private_subnets[idx] }
+  for_each = { for idx, AZ in local.AZs : AZ => local.private_subnets[idx] }
   vpc_id            = aws_vpc.main.id
   cidr_block        = each.value
   availability_zone = each.key
@@ -53,14 +53,14 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = values(aws_subnet.public)[0].id
   tags = merge(var.tags, { Name = "${var.environment_name}-nat" })
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.internet_gateway]
 }
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
   tags = merge(var.tags, { Name = "${var.environment_name}-public-rt" })
 }
