d6867076b94549d7cda419e9b33cb8885cd39e6a
arslan-ashraf <arslanash@gmail.com>
2026-07-17 15:35:20 -0400

    changes

--- 37-eks-cluster/vpc-network/module/main.tf
+++ 37-eks-cluster/vpc-network/module/main.tf
@@ -56,7 +56,7 @@ resource "aws_nat_gateway" "nat_gateway" {
   depends_on = [aws_internet_gateway.internet_gateway]
 }
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -68,7 +68,7 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_rt_assoc" {
   for_each = aws_subnet.public
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table" "private_rt" {
