ca6ac909c261eb81d647b0ca8d2d5ffca45bd1f6
arslan-ashraf <arslanash@gmail.com>
2026-07-17 15:43:56 -0400

    changes

--- 37-eks-cluster/vpc-network/module/vpc-network.tf
+++ 37-eks-cluster/vpc-network/module/vpc-network.tf
@@ -8,40 +8,6 @@ locals {
   }
 }
 
-# if the subnet_config defined in the module file variables.tf looks like this:
-# subnet_config = {
-#   subnet_a = {
-#     cidr_block = "10.0.1.0/24"
-#     public     = true
-#     AZ         = "us-east-1a"
-#   }
-#   subnet_b = {
-#     cidr_block = "10.0.2.0/24"
-#     public     = false
-#     AZ         = "us-east-1b"
-#   }
-#   subnet_c = {
-#     cidr_block = "10.0.3.0/24"
-#     public     = true
-#     AZ         = "us-east-1c"
-#   }
-# }
-
-# then the local.public_subnets at the top will return like this:
-# public_subnets = {
-#   subnet_a = {
-#     cidr_block = "10.0.1.0/24"
-#     public     = true
-#     AZ         = "us-east-1a"
-#   }
-#   subnet_c = {
-#     cidr_block = "10.0.3.0/24"
-#     public     = true
-#     AZ         = "us-east-1c"
-#   }
-# }
-
-
 data "aws_availability_zones" "AZs" {
   state = "available"
 }
