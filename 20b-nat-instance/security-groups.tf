d3bd6bce0deab687f965bd582521cfe7096b6000
arslan-ashraf <arslanash@gmail.com>
2026-07-17 18:54:14 -0400

    changes

--- 20b-nat-instance/security-groups.tf
+++ 20b-nat-instance/security-groups.tf
@@ -69,29 +69,22 @@ resource "aws_security_group" "security_group_for_NAT_instance" {
   tags   = { Name = "security_group_for_NAT_instance" }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
+resource "aws_vpc_security_group_ingress_rule" "ingress_from_ec2_rule" {
   security_group_id = aws_security_group.security_group_for_NAT_instance.id
   
   # where is the traffic coming from
-  referenced_security_group_id = aws_security_group.security_group_for_ec2_instance_endpoint.id
-
-  from_port = 22
-  to_port   = 22
+  referenced_security_group_id = aws_security_group.security_group_for_ec2_instance.id
 
   ip_protocol = "-1"
 }
 
-  ingress {
-    protocol    = "-1"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-    from_port   = 0
-    to_port     = 0
-  }
+# allow traffic out to the internet
+resource "aws_vpc_security_group_egress_rule" "egress_internet_rule" {
+  security_group_id = aws_security_group.security_group_for_NAT_instance.id
 
-  egress {
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    to_port     = 0
-  }
+  # Target destination
+  cidr_ipv4 = "0.0.0.0/0"
+
+  # all protocols
+  ip_protocol = "-1"
 }
