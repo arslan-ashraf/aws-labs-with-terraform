80226aa394be4fced035e59e0dfe030ccb91b236
arslan-ashraf <arslanash@gmail.com>
2026-07-17 15:44:05 -0400

    changes

--- 37-eks-cluster/vpc-network/module/variables.tf
+++ 37-eks-cluster/vpc-network/module/variables.tf
@@ -24,22 +24,3 @@ variable "subnet_config" {
     error_message = "The subnet cidr_block must be valid."
   }
 }
-
-# the subnet_config map defined above would look like this:
-# subnet_config = {
-#   subnet_a = {
-#     cidr_block = "10.0.1.0/24"
-#     public     = true
-#     AZ         = "us-east-1a"
-#   }
-#   subnet_b = {
-#     cidr_block = "10.0.2.0/24"
-#     public     = false
-#     AZ         = "us-east-1b"
-#   }
-#   subnet_c = {
-#     cidr_block = "10.0.3.0/24"
-#     public     = true
-#     AZ         = "us-east-1c"
-#   }
-# }
