646e51292e7e2199999cde446cf7f07368b7fdfd
arslan-ashraf <arslanash@gmail.com>
2026-07-17 17:02:18 -0400

    changes

--- 37-eks-cluster/vpc-network/module/vpc-network.tf
+++ 37-eks-cluster/vpc-network/module/vpc-network.tf
@@ -94,3 +94,17 @@ resource "aws_route_table_association" "rtb_public_subnets_assoc" {
 ############### ROUTE TABLE FOR PRIVATE SUBNETS ###################
 ###################################################################
 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt" })
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  for_each = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
+}
