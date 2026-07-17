c890c119ebf2ca5a117e86b6bc6ab2abdb9fa97c
arslan-ashraf <arslanash@gmail.com>
2026-07-17 16:47:42 -0400

    changes

--- 37-eks-cluster/vpc-network/module/vpc-network.tf
+++ 37-eks-cluster/vpc-network/module/vpc-network.tf
@@ -42,6 +42,29 @@ resource "aws_internet_gateway" "main_internet_gateway" {
 }
 
 ###################################################################
+########################## NAT GATEWAY ############################
+###################################################################
+
+# allocate elastic IP (EIP) for the nat gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.main_internet_gateway]
+
+  tags = { Name = "nat_gateway_elastic_ip" }
+}
+
+# create the NAT Gateway in public subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet_for_nat_gateway.id
+
+  tags = { Name = "nat_gateway" }
+
+  # Explicit dependency to ensure proper ordering during creation/destruction
+  depends_on = [aws_internet_gateway.internet_gateway_for_example_vpc]
+}
+
+###################################################################
 ############### ROUTE TABLE FOR PUBLIC SUBNETS ####################
 ###################################################################
 
