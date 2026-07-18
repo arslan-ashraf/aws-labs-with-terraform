fcf0f8dab3c6f97069269bb20886ed2fbe8d2b74
arslan-ashraf <arslanash@gmail.com>
2026-07-18 10:01:26 -0400

    changes

--- 35-ec2-auto-scaling/ec2-launch-template.tf
+++ 35-ec2-auto-scaling/ec2-launch-template.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "public_private_key_pair" {
+resource "aws_key_pair" "public_SSH_key" {
   key_name   = "key-for-ec2-connection"
   public_key = file("~/.ssh/key-for-ec2-connection.pub")
 }
@@ -8,7 +8,7 @@ resource "aws_launch_template" "ec2_auto_scaling_launch_template" {
 
   image_id      = "ami-0ec10929233384c7f" # ubuntu ami
   instance_type = "t2.nano"
-  key_name      = aws_key_pair.public_private_key_pair.key_name
+  key_name      = aws_key_pair.public_SSH_key.key_name
 
   # vpc_security_group_ids = [
   #   aws_security_group.security_group_public_traffic.id
