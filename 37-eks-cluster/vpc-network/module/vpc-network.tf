942c7c7dab41f80399f0e91527127b65a15fdb1c
arslan-ashraf <arslanash@gmail.com>
2026-07-17 23:31:37 -0400

    changes

--- 37-eks-cluster/vpc-network/module/vpc-network.tf
+++ 37-eks-cluster/vpc-network/module/vpc-network.tf
@@ -105,10 +105,15 @@ resource "aws_route_table_association" "rtb_public_subnets_assoc" {
 # route all outbound private subnet EC2 traffic to the NAT Gateway
 resource "aws_route_table" "route_table_for_private_subnets" {
   vpc_id = aws_vpc.main.id
+
   route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+    # destination
+    cidr_block = "0.0.0.0/0"
+
+    # target to reach destintion
+    network_interface_id = aws_instance.nat_instance.primary_network_interface_id
   }
+  
   tags = { Name = "route_table_for_private_subnets" }
 }
 
