7a8ed1a8205a167f15ae21e96980b61f1110f554
arslan-ashraf <arslanash@gmail.com>
2026-07-18 20:11:13 -0400

    changes

---
+++ 37-eks-cluster/eks-cluster/eks-cluster.tf
@@ -0,0 +1,15 @@
+resource "aws_eks_cluster" "this" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["subnet-abcde123", "subnet-bcdef234"]
+  }
+
+  # CRITICAL: Always use depends_on for the policy attachment. 
+  # If the attachment isn't fully created first, cluster provisioning will fail.
+  # If deleted before the cluster during a destroy, EKS won't be able to clean up security groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
