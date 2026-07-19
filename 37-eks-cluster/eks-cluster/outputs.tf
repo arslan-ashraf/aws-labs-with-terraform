bcee932a52ae85cade9d27de67b9a95c8fb4fc67
arslan-ashraf <arslanash@gmail.com>
2026-07-18 21:39:15 -0400

    changes

--- 37-eks-cluster/eks-cluster/outputs.tf
+++ 37-eks-cluster/eks-cluster/outputs.tf
@@ -3,9 +3,16 @@ output "to_configure_kubectl" {
   value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
 }
 
+# EKS Cluster API server endpoint
+# used by kubectl and external tools to communicate with the cluster
+output "eks_cluster_endpoint" {
+  value       = aws_eks_cluster.eks_cluster.endpoint
+  description = "EKS API Server endpoint"
+}
+
 # output the EKS Cluster Certificate Authority data
 # its needed when setting up kubeconfig or accessing EKS via API
 output "eks_cluster_certificate_authority_data" {
-  value       = aws_eks_cluster.main.certificate_authority[0].data
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
   description = "Base64 encoded CA certificate for kubectl config"
 }
