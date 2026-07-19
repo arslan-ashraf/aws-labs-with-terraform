1d0ac8038035a0082a4d7474becf93113aabdf84
arslan-ashraf <arslanash@gmail.com>
2026-07-18 20:20:50 -0400

    changes

--- 37-eks-cluster/eks-cluster/eks-cluster.tf
+++ 37-eks-cluster/eks-cluster/eks-cluster.tf
@@ -1,5 +1,5 @@
-resource "aws_eks_cluster" "this" {
-  name     = "my-cluster"
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks_cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
