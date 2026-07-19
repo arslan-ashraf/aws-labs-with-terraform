7f7e1140ac43d9ff7c49eb95243e21a3c84d8707
arslan-ashraf <arslanash@gmail.com>
2026-07-18 21:37:22 -0400

    changes

--- 37-eks-cluster/eks-cluster/outputs.tf
+++ 37-eks-cluster/eks-cluster/outputs.tf
@@ -1,4 +1,4 @@
 output "to_configure_kubectl" {
   description = "Command to update local kubeconfig to connect to the EKS cluster"
-  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${local.eks_cluster_name}"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
 }
