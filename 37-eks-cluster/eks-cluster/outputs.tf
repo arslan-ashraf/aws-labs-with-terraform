78170246b1fdf83c26908898f268fdd54846de1f
arslan-ashraf <arslanash@gmail.com>
2026-07-18 21:38:24 -0400

    changes

--- 37-eks-cluster/eks-cluster/outputs.tf
+++ 37-eks-cluster/eks-cluster/outputs.tf
@@ -2,3 +2,10 @@ output "to_configure_kubectl" {
   description = "Command to update local kubeconfig to connect to the EKS cluster"
   value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
 }
+
+# output the EKS Cluster Certificate Authority data
+# its needed when setting up kubeconfig or accessing EKS via API
+output "eks_cluster_certificate_authority_data" {
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+  description = "Base64 encoded CA certificate for kubectl config"
+}
