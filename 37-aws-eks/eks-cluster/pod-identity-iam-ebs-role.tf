81657e1bab1f5dab7e9dc303808e790df06e15d4
arslan-ashraf <arslanash@gmail.com>
2026-07-24 19:34:43 -0400

    changes

--- 37-aws-eks/eks-cluster/pod-identity-iam-ebs-role.tf
+++ 37-aws-eks/eks-cluster/pod-identity-iam-ebs-role.tf
@@ -5,12 +5,7 @@ resource "aws_iam_role" "pod_identity_ebs_volumes_role" {
   assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
 }
 
-resource "aws_iam_policy" "read_secrets_policy" {
-  name   = "secrets-manager-read-policy"
-  policy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
 resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
   role       = aws_iam_role.pod_identity_ebs_volumes_role.name
-  policy_arn = aws_iam_policy_document.read_secrets_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
