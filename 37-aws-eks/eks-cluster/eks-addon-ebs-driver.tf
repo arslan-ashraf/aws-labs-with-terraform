df29f2f2caf108b32a315effec321d1eb767b435
arslan-ashraf <arslanash@gmail.com>
2026-07-24 19:26:51 -0400

    changes

--- 37-aws-eks/eks-cluster/eks-addon-ebs-driver.tf
+++ 37-aws-eks/eks-cluster/eks-addon-ebs-driver.tf
@@ -1,10 +1,7 @@
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = var.eks_cluster_name
-  addon_name   = "eks-pod-identity-agent"
+  addon_name   = "aws-ebs-csi-driver"
 
   # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
   # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
 }
-
-
-aws-ebs-csi-driver
