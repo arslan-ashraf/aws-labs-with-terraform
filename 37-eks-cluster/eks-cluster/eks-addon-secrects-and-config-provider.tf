9e73aed8fbd919418ba91edb074ad998ad683884
arslan-ashraf <arslanash@gmail.com>
2026-07-24 15:32:36 -0400

    changes

--- 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
+++ 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
@@ -7,4 +7,7 @@
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = var.eks_cluster_name
   addon_name   = "aws-secrets-store-csi-driver-provider"
+
+
+  # configuration_values { ... }
 }
