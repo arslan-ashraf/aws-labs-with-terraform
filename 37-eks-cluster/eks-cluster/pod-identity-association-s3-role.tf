83587c14096e0033d87aa74320809df486360187
arslan-ashraf <arslanash@gmail.com>
2026-07-24 15:12:58 -0400

    changes

--- 37-eks-cluster/eks-cluster/pod-identity-association-s3-role.tf
+++ 37-eks-cluster/eks-cluster/pod-identity-association-s3-role.tf
@@ -4,5 +4,6 @@ resource "aws_eks_pod_identity_association" "app_association" {
   service_account = "example-service-account"
   role_arn        = aws_iam_role.pod_identity_S3_read_only_role.arn
 
+  # wait for the pod identity agent addon to be created first
   depends_on      = [aws_eks_addon.pod_identity_agent]
 }
