9942a1933047eb356a21e3700554665fdf96e584
arslan-ashraf <arslanash@gmail.com>
2026-07-24 15:29:40 -0400

    changes

--- 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
+++ 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
@@ -1,3 +1,7 @@
+# note: the AWS ASCP (Amazon Secrets and Configuration Provider)
+# automatically also installs the Kubernetes native pluggin (driver)
+# Secrets Store CSI Driver
+
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = var.eks_cluster_name
   addon_name   = "aws-secrets-store-csi-driver-provider"
