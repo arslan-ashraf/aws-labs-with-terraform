d2022ce6e42bfa402b5196fef20bddcc5f63acd1
arslan-ashraf <arslanash@gmail.com>
2026-07-24 16:08:20 -0400

    changes

--- 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
+++ 37-eks-cluster/eks-cluster/eks-addon-secrects-and-config-provider.tf
@@ -8,7 +8,8 @@ resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name = var.eks_cluster_name
   addon_name   = "aws-secrets-store-csi-driver-provider"
 
-  # resolve_conflicts_on_create = "OVERWRITE" # during updates
+  # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
+  # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
 
   # use this to configure the CSI Driver
   # configuration_values = jsonencode({
