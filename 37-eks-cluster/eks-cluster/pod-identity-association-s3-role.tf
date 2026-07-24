eab324d94d41da002119c42f5803e294ea769c64
arslan-ashraf <arslanash@gmail.com>
2026-07-24 15:11:22 -0400

    changes

--- 37-eks-cluster/eks-cluster/pod-identity-association-s3-role.tf
+++ 37-eks-cluster/eks-cluster/pod-identity-association-s3-role.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_pod_identity_association" "app_association" {
-  cluster_name    = var.cluster_name
+  cluster_name    = var.eks_cluster_name
   namespace       = "production"
-  service_account = "my-application-sa"
+  service_account = "example-service-account"
   role_arn        = aws_iam_role.app_role.arn
 }
