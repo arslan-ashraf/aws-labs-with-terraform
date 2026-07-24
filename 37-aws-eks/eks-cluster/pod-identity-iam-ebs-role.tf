00bdb22cf89e31a1e3015a7e60178c4830e3bd69
arslan-ashraf <arslanash@gmail.com>
2026-07-24 19:34:05 -0400

    changes

--- 37-aws-eks/eks-cluster/pod-identity-iam-ebs-role.tf
+++ 37-aws-eks/eks-cluster/pod-identity-iam-ebs-role.tf
@@ -1,29 +1,16 @@
 resource "aws_iam_role" "pod_identity_ebs_volumes_role" {
-  name               = "pod_identity_secrets_manager_role"
+  name               = "pod_identity_ebs_volumes_role"
 
   # pod_identity_trust_policy has already been defined
   assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
 }
 
-data "aws_iam_policy_document" "read_secrets_policy_document" {
-  statement {
-    effect = "Allow"
-        
-    actions = [
-      "secretsmanager:GetSecretValue",
-      "secretsmanager:DescribeSecret"
-    ]
-
-    resources = [var.database_secrets_arn]
-  }
-}
-
 resource "aws_iam_policy" "read_secrets_policy" {
   name   = "secrets-manager-read-policy"
-  policy = data.aws_iam_policy_document.read_secrets_policy_document.json
+  policy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
-  role       = aws_iam_role.pod_identity_secrets_manager_role.name
+  role       = aws_iam_role.pod_identity_ebs_volumes_role.name
   policy_arn = aws_iam_policy_document.read_secrets_policy.arn
 }
