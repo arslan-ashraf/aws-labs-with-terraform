0d2c4b539402aada69f73345a2767ccef32eecde
arslan-ashraf <arslanash@gmail.com>
2026-07-24 16:20:55 -0400

    changes

--- 37-eks-cluster/eks-cluster/pod-identity-association-read-secrets-role.tf
+++ 37-eks-cluster/eks-cluster/pod-identity-association-read-secrets-role.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_pod_identity_association" "app_association" {
+resource "aws_eks_pod_identity_association" "service_account_secrets_iam_assoc" {
   cluster_name    = var.eks_cluster_name
   namespace       = "production"
   service_account = "aws-cli-service-account"
