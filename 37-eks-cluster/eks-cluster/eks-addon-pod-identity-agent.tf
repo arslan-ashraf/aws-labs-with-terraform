ebc017a08f7fdb2b46dba5de733280624773934e
arslan-ashraf <arslanash@gmail.com>
2026-07-24 15:25:35 -0400

    changes

--- 37-eks-cluster/eks-cluster/eks-addon-pod-identity-agent.tf
+++ 37-eks-cluster/eks-cluster/eks-addon-pod-identity-agent.tf
@@ -5,11 +5,11 @@ resource "aws_eks_addon" "pod_identity_agent" {
 
 # to get the addon_name, run the command:
 
-aws eks describe-addon-versions  \
-   --query 'sort_by(addons  &owner)[].{
-    publisher: publisher, 
-    owner: owner, 
-    addonName: addonName, 
-    type: type
-  }' \
-   --output table
+# aws eks describe-addon-versions  \
+#    --query 'sort_by(addons  &owner)[].{
+#     publisher: publisher, 
+#     owner: owner, 
+#     addonName: addonName, 
+#     type: type
+#   }' \
+#    --output table
