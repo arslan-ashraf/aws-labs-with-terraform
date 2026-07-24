082f87c3a7f29deb36134ec36d42101b15f7efde
arslan-ashraf <arslanash@gmail.com>
2026-07-24 19:32:49 -0400

    changes

---
+++ 37-aws-eks/eks-cluster/pod-identity-iam-ebs-role.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "pod_identity_ebs_volumes_role" {
+  name               = "pod_identity_secrets_manager_role"
+
+  # pod_identity_trust_policy has already been defined
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
+}
+
+data "aws_iam_policy_document" "read_secrets_policy_document" {
+  statement {
+    effect = "Allow"
+        
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+
+    resources = [var.database_secrets_arn]
+  }
+}
+
+resource "aws_iam_policy" "read_secrets_policy" {
+  name   = "secrets-manager-read-policy"
+  policy = data.aws_iam_policy_document.read_secrets_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.pod_identity_secrets_manager_role.name
+  policy_arn = aws_iam_policy_document.read_secrets_policy.arn
+}
