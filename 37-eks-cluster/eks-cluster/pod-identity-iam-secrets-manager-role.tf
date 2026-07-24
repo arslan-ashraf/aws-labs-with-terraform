d89420ebdc245e3b76af0788dc25a4e3b4b82974
arslan-ashraf <arslanash@gmail.com>
2026-07-24 16:13:11 -0400

    changes

--- 37-eks-cluster/eks-cluster/pod-identity-iam-secrets-manager-role.tf
+++ 37-eks-cluster/eks-cluster/pod-identity-iam-secrets-manager-role.tf
@@ -10,8 +10,8 @@ data "aws_iam_policy_document" "read_secrets_policy" {
     effect = "Allow"
 
     actions = [
-      "sts:AssumeRole",
-      "sts:TagSession"
+      "secretsmanager:AssumeRole",
+      "secretsmanager:TagSession"
     ]
   }
 }
