a6c45c56a02c7798bfee64f4d8ddca35f5aee07c
arslan-ashraf <arslanash@gmail.com>
2026-07-25 18:41:43 -0400

    changes

--- 37-aws-eks/eks-cluster/load-balancer-controller-iam-role.tf
+++ 37-aws-eks/eks-cluster/load-balancer-controller-iam-role.tf
@@ -1,3 +1,10 @@
+resource "aws_iam_role" "pod_identity_ebs_volumes_role" {
+  name = "pod_identity_ebs_volumes_role"
+
+  # pod_identity_trust_policy has already been defined
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
+}
+
 // the IAM policy used here is from the K8s-sigs, download it with:
 // curl -o load-balancer-controller-iam-policy.json \
 // https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.14.0/docs/install/iam_policy.json
