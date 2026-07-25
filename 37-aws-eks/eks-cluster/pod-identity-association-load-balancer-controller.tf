4556595d026ba7ff6688add2fc90ae8d97b77d2a
arslan-ashraf <arslanash@gmail.com>
2026-07-25 18:54:20 -0400

    changes

---
+++ 37-aws-eks/eks-cluster/pod-identity-association-load-balancer-controller.tf
@@ -0,0 +1,14 @@
+# pod identity association needs three things:
+# 1. the corresponding addon to be installed in the eks cluster
+# 2. the IAM role with permissions to reach out to AWS services
+# 3. the correct k8s service account in the correct namespace
+
+resource "aws_eks_pod_identity_association" "service_account_s3_iam_assoc" {
+  cluster_name    = var.eks_cluster_name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.load_balancer_controller_role.arn
+
+  # wait for the pod identity agent addon to be created first
+  depends_on = [aws_eks_addon.pod_identity_agent]
+}
