2b067732ddd467b7d41bc727ddac96306f6c1445
arslan-ashraf <arslanash@gmail.com>
2026-07-24 20:45:51 -0400

    changes

--- 37-aws-eks/eks-cluster/eks-addon-ebs-driver.tf
+++ 37-aws-eks/eks-cluster/eks-addon-ebs-driver.tf
@@ -5,4 +5,7 @@ resource "aws_eks_addon" "ebs_csi_driver"
 
   # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
   # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
+
+  # ensure the Pod Identity Association is created first
+  depends_on = [aws_eks_pod_identity_association.ebs_csi]
 }
