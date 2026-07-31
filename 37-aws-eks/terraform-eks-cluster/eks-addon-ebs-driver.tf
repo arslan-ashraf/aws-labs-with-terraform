825c536ce14e3e25fc3d9a9428cf425df1d0d551
arslan-ashraf <arslanash@gmail.com>
2026-07-31 16:32:43 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/eks-addon-ebs-driver.tf
+++ 37-aws-eks/terraform-eks-cluster/eks-addon-ebs-driver.tf
@@ -2,12 +2,11 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name = var.eks_cluster_name
   addon_name   = "aws-ebs-csi-driver"
 
-  # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
-  # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
+  resolve_conflicts_on_create = "OVERWRITE" # concerning versions
+  resolve_conflicts_on_update = "OVERWRITE" # concerning versions
 
   depends_on = [
     aws_eks_addon.pod_identity_agent,
     aws_eks_node_group.private_nodes
   ]
-
 }
