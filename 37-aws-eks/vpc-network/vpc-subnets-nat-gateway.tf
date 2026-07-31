2773d20099b5977d7bb9eb6c5a58245356253332
arslan-ashraf <arslanash@gmail.com>
2026-07-31 15:02:11 -0400

    changes

--- 37-aws-eks/vpc-network/vpc-subnets-nat-gateway.tf
+++ 37-aws-eks/vpc-network/vpc-subnets-nat-gateway.tf
@@ -9,12 +9,12 @@ module "vpc_network_module" {
   subnet_config = {
     # subnets a and b are private for hosting EKS worker nodes
     private_subnet_a = {
-      cidr_block = "10.0.1.0/24"
+      cidr_block = "10.0.0.0/24"
       public     = false
       AZ         = "us-east-1a"
     }
     private_subnet_b = {
-      cidr_block = "10.0.2.0/24"
+      cidr_block = "10.0.1.0/24"
       public     = false
       AZ         = "us-east-1b"
     }
@@ -26,6 +26,17 @@ module "vpc_network_module" {
       AZ                               = "us-east-1a"
       public                           = true
       contains_NAT_instance            = true
+      contains_external_load_balancer  = true
+      contains_internal_load_balancer  = false
+    }
+
+    # subnet d is public, it is for hosting public facing resources
+    # such as Load Balancers which require at least two AZs
+    public_subnet_d = {
+      cidr_block                       = "10.0.11.0/24"
+      AZ                               = "us-east-1b"
+      public                           = true
+      contains_NAT_instance            = false
       contains_external_load_balancer  = true
       contains_internal_load_balancer  = false
     }
