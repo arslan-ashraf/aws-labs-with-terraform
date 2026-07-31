f99fdd4a90bff007acc817c9bded1848099790ff
arslan-ashraf <arslanash@gmail.com>
2026-07-31 15:11:57 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/providers.tf
+++ 37-aws-eks/terraform-eks-cluster/providers.tf
@@ -6,9 +6,35 @@ terraform {
       version = "~> 6.55.0"
     }
 
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.2.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+# HELM Provider
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+# Terraform Kubernetes Provider
+provider "kubernetes" {
+  host = aws_eks_cluster.main.endpoint 
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster.token
 }
