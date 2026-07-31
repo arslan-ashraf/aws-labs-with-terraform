0936231b76d68a73ea58f417dd9854a74876d90d
arslan-ashraf <arslanash@gmail.com>
2026-07-31 16:32:33 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/eks-addon-ebs-driver.tf
+++ 37-aws-eks/terraform-eks-cluster/eks-addon-ebs-driver.tf
@@ -5,7 +5,6 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
   # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
 
-  # Eensure the Pod Identity Association is created first
   depends_on = [
     aws_eks_addon.pod_identity_agent,
     aws_eks_node_group.private_nodes
