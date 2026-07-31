a93ec9e785a430c966e850409eb2d7f84ae93abd
arslan-ashraf <arslanash@gmail.com>
2026-07-31 16:21:21 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/eks-addon-secrets-and-config-provider.tf
+++ 37-aws-eks/terraform-eks-cluster/eks-addon-secrets-and-config-provider.tf
@@ -8,8 +8,8 @@ resource "aws_eks_addon" "ASCP_and_CSI_driver" {
   cluster_name = var.eks_cluster_name
   addon_name   = "aws-secrets-store-csi-driver-provider"
 
-  # resolve_conflicts_on_create = "OVERWRITE" # concerning versions
-  # resolve_conflicts_on_update = "OVERWRITE" # concerning versions
+  resolve_conflicts_on_create = "OVERWRITE" # concerning versions
+  resolve_conflicts_on_update = "OVERWRITE" # concerning versions
 
   # use this to configure the Secrets Store CSI Driver:
   # configuration_values = jsonencode({
