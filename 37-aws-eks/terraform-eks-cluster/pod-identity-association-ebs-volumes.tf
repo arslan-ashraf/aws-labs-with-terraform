faaee5384777663ff0db3a31844dcd15540a47f8
arslan-ashraf <arslanash@gmail.com>
2026-07-31 16:31:01 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/pod-identity-association-ebs-volumes.tf
+++ 37-aws-eks/terraform-eks-cluster/pod-identity-association-ebs-volumes.tf
@@ -3,7 +3,7 @@
 # 2. the IAM role with permissions to reach out to AWS services
 # 3. the correct k8s service account in the correct namespace
 
-resource "aws_eks_pod_identity_association" "service_account_ebs_iam_assoc" {
+resource "aws_eks_pod_identity_association" "EBS_Volumes_PIA" {
   cluster_name    = var.eks_cluster_name
   namespace       = "kube-system"
   service_account = "ebs-csi-controller-sa"
