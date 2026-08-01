8812b581557bfb6aced05a7311fc858b0e5ffff6
arslan-ashraf <arslanash@gmail.com>
2026-08-01 17:40:45 -0400

    changes

---
+++ 50-minimal-eks/vpc-network.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "minimal_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.minimal_vpc.id
+}
+
+# Subnet 1
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.minimal_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+# Subnet 2
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.minimal_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.minimal_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.subnet_b.id
+  route_table_id = aws_route_table.rt.id
+}
