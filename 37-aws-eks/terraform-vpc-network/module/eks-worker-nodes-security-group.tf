f478f2d79008d00cb8ba11c598e07ff8ff070a35
arslan-ashraf <arslanash@gmail.com>
2026-07-31 22:35:43 -0400

    changes

--- 37-aws-eks/terraform-vpc-network/module/eks-worker-nodes-security-group.tf
+++ 37-aws-eks/terraform-vpc-network/module/eks-worker-nodes-security-group.tf
@@ -29,23 +29,13 @@ resource "aws_security_group_ingress_rule" "nodes_internal_communication" {
   protocol                 = "-1"
 }
 
-# Allow cluster control plane to reach kubelet, webhooks, and other node services
-resource "aws_security_group_rule" "nodes_ingress_from_cluster" {
-  description              = "Allow cluster to communicate with nodes"
-  
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.eks_cluster.id
-  security_group_id        = aws_security_group.eks_nodes.id
-  type                     = "ingress"
-}
-
-
 # allow EKS control plane to communicate with worker nodes
+# to reach kubelet, webhooks, and other node services
 resource "aws_vpc_security_group_ingress_rule" "nodes_inbound_from_control_plane_rule" {
   security_group_id        = aws_security_group.eks_worker_nodes_security_group.id
   
   # where the traffic is coming from
-  referenced_security_group_id = aws_security_group.eks_cluster.id
+  referenced_security_group_id = aws_security_group.eks_cluster_security_group.id
 
   from_port                = 443
   to_port                  = 65535
