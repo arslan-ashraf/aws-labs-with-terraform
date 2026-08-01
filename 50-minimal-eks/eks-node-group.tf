4b5e1af409a8f89770ec956aba89119e6a5ef866
arslan-ashraf <arslanash@gmail.com>
2026-08-01 18:04:40 -0400

    changes

--- 50-minimal-eks/eks-node-group.tf
+++ 50-minimal-eks/eks-node-group.tf
@@ -1,6 +1,6 @@
 # EKS Managed Node Group (1 Instance)
-resource "aws_eks_node_group" "minimal_nodes" {
-  cluster_name    = aws_eks_cluster.minimal_cluster.name
+resource "aws_eks_node_group" "eks_worker_nodes" {
+  cluster_name    = aws_eks_cluster.example_eks_cluster.name
   node_group_name = "minimal-node-group"
   node_role_arn   = aws_iam_role.node_role.arn
   subnet_ids      = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
