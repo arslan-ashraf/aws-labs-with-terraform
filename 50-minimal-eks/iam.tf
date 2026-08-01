65350438ecd0f6269869e3b55d2a94cef7e40bb2
arslan-ashraf <arslanash@gmail.com>
2026-08-01 17:53:12 -0400

    changes

--- 50-minimal-eks/iam.tf
+++ 50-minimal-eks/iam.tf
@@ -1,38 +1,20 @@
-data "aws_iam_policy_document" "eks_trust_policy_document" {
-  statement {
-    effect = "Allow"
+# resource "aws_iam_role" "cluster_role" {
+#   name = "minimal-eks-cluster-role"
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = { Service = "://amazonaws.com" }
+#     }]
+#   })
+# }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name               = "eks_cluster_role"
-  assume_role_policy = data.aws_iam_policy_document.eks_trust_policy_document.json
-}
-
-resource "aws_iam_role" "cluster_role" {
-  name = "minimal-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "://amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster_role.name
-}
+# resource "aws_iam_role_policy_attachment" "cluster_policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role       = aws_iam_role.cluster_role.name
+# }
 
 # IAM Role for Node Group
 resource "aws_iam_role" "node_role" {
