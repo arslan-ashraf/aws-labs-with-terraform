7bd2d79105ff3be0c2eb8baac575241a76d7ed86
arslan-ashraf <arslanash@gmail.com>
2026-08-01 17:43:09 -0400

    changes

--- 50-minimal-eks/vpc-network.tf
+++ 50-minimal-eks/vpc-network.tf
@@ -1,33 +1,30 @@
-resource "aws_vpc" "minimal_vpc" {
+resource "aws_vpc" "vpc_for_eks" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+  tags = { Name = "vpc_for_eks" }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.minimal_vpc.id
+  vpc_id = aws_vpc.vpc_for_eks.id
 }
 
-# Subnet 1
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.minimal_vpc.id
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.vpc_for_eks.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
-# Subnet 2
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.minimal_vpc.id
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.vpc_for_eks.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
-# Route Table
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.minimal_vpc.id
+resource "aws_route_table" "route_table_eks_vpc" {
+  vpc_id = aws_vpc.vpc_for_eks.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -35,13 +32,13 @@ resource "aws_route_table" "rt" {
   }
 }
 
-# Route Table Associations
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet_a.id
-  route_table_id = aws_route_table.rt.id
+
+resource "aws_route_table_association" "public_subnet_a_assoc" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.route_table_eks_vpc.id
 }
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.subnet_b.id
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "public_subnet_b_assoc" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.route_table_eks_vpc.id
 }
