950d7e22c848139c0f3ccdc9c6fdbe1b7f8dfa7e
arslan-ashraf <arslanash@gmail.com>
2026-07-31 22:49:13 -0400

    changes

--- 37-aws-eks/terraform-eks-cluster/eks-node-group-ec2-launch-template.tf
+++ 37-aws-eks/terraform-eks-cluster/eks-node-group-ec2-launch-template.tf
@@ -1,10 +1,9 @@
 resource "aws_launch_template" "eks_nodes_launch_template" {
   name          = "eks_nodes_launch_template"
-  description   = "Custom Launch Template for EKS Managed Node Group"
-  instance_type = "t3.nano"
+  instance_type = var.node_instance_types
 
   vpc_security_group_ids = [
-    data.terraform_remote_state.vpc_network.outputs.security_group_for_EKS_node_group_id
+    data.terraform_remote_state.vpc_network.outputs.eks_worker_nodes_security_group_id
   ]
 
   # Best Practice: Force IMDSv2 for security compliance
