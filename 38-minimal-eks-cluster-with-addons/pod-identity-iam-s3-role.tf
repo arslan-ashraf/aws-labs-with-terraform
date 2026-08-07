2bb497f324bea87666471892986925edb68615f4
arslan-ashraf <arslanash@gmail.com>
2026-08-07 17:21:31 -0400

    changes

--- 38-minimal-eks-cluster-with-addons/pod-identity-iam-s3-role.tf
+++ 38-minimal-eks-cluster-with-addons/pod-identity-iam-s3-role.tf
@@ -1,26 +1,26 @@
-# data "aws_iam_policy_document" "pod_identity_trust_policy" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "pod_identity_trust_policy" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["pods.eks.amazonaws.com"]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
 
-#     actions = [
-#       "sts:AssumeRole",
-#       "sts:TagSession"
-#     ]
-#   }
-# }
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
 
-# resource "aws_iam_role" "pod_identity_S3_read_only_role" {
-#   name               = "pod_identity_S3_read_only_role"
-#   assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
-# }
+resource "aws_iam_role" "pod_identity_S3_read_only_role" {
+  name               = "pod_identity_S3_read_only_role"
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_trust_policy.json
+}
 
 
-# resource "aws_iam_role_policy_attachment" "s3_read_role_policy_attachment" {
-#   role       = aws_iam_role.pod_identity_S3_read_only_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-# }
+resource "aws_iam_role_policy_attachment" "s3_read_role_policy_attachment" {
+  role       = aws_iam_role.pod_identity_S3_read_only_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
