0be8252bbc7faf358cc49bf4572f7f705222287f
arslan-ashraf <arslanash@gmail.com>
2026-08-07 17:33:53 -0400

    changes

---
+++ 38-minimal-eks-cluster-with-addons/eks-addon-secrets-and-config-provider.tf
@@ -0,0 +1,50 @@
+data "aws_eks_addon_version" "ASCP_CSI_Driver_default_version" {
+  addon_name         = "aws-secrets-store-csi-driver-provider"
+  kubernetes_version = aws_eks_cluster.example_eks_cluster.version
+}
+
+
+# get latest ASCP version compatible with EKS cluster's version
+data "aws_eks_addon_version" "ASCP_CSI_Driver_lastest" {
+  addon_name         = "aws-secrets-store-csi-driver-provider"
+  kubernetes_version = aws_eks_cluster.example_eks_cluster.version
+  most_recent        = true
+}
+
+
+
+# note: the AWS ASCP (Amazon Secrets and Configuration Provider)
+# automatically also installs the Kubernetes native pluggin (driver)
+# Secrets Store CSI Driver, AWS ASCP pulls secrets from AWS Secrets
+# Manager and hands them over to CSI Driver which then mounts those
+# secrets into ephemeral in-memory temporary volumes through the 
+# custom object SecretProviderClass
+resource "aws_eks_addon" "ASCP_and_CSI_driver" {
+  cluster_name  = var.eks_cluster_name
+  addon_name    = "aws-secrets-store-csi-driver-provider"
+  addon_version = data.aws_eks_addon_version.ASCP_CSI_Driver_lastest.version
+
+  resolve_conflicts_on_create = "OVERWRITE" # concerning versions
+  resolve_conflicts_on_update = "OVERWRITE" # concerning versions
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_eks_node_group.private_nodes
+  ]
+
+  # use this to configure the Secrets Store CSI Driver:
+  # configuration_values = jsonencode({
+  #   secrets-store-csi-driver = {}
+  #   replicaCount = 4
+  #   resources = {
+  #     limits = {
+  #       cpu    = "100m"
+  #       memory = "150Mi"
+  #     }
+  #     requests = {
+  #       cpu    = "100m"
+  #       memory = "150Mi"
+  #     }
+  #   }
+  # })
+}
