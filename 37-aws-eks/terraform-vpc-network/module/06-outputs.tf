8535cbf88254335209e78d9d09bcde339cbc1ce8
arslan-ashraf <arslanash@gmail.com>
2026-08-07 15:30:29 -0400

    changes

--- 37-aws-eks/terraform-vpc-network/module/06-outputs.tf
+++ 37-aws-eks/terraform-vpc-network/module/06-outputs.tf
@@ -50,12 +50,12 @@ output "NAT_instance_subnet_names" {
   value = keys(local.public_subnets_for_NAT_instance)
 }
 
-output "eks_cluster_security_group_id" {
-  value       = aws_security_group.eks_cluster_security_group.id
-  description = "Security group for EKS cluster control plane."
-}
+# output "eks_cluster_security_group_id" {
+#   value       = aws_security_group.eks_cluster_security_group.id
+#   description = "Security group for EKS cluster control plane."
+# }
 
-output "eks_worker_nodes_security_group_id" {
-  value       = aws_security_group.eks_worker_nodes_security_group.id
-  description = "Security group for EKS worker nodes with outbound access to NAT Instance."
-}
+# output "eks_worker_nodes_security_group_id" {
+#   value       = aws_security_group.eks_worker_nodes_security_group.id
+#   description = "Security group for EKS worker nodes with outbound access to NAT Instance."
+# }
