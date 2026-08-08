fa47ecfbe0ea170a8e69eb7dd2df660f6f534150
arslan-ashraf <arslanash@gmail.com>
2026-08-08 14:48:38 -0400

    changes

--- 40-secure-eks-cluster-with-vpc-endpoints/04-vpc-endpoint-names.tf
+++ 40-secure-eks-cluster-with-vpc-endpoints/04-vpc-endpoint-names.tf
@@ -1,5 +1,6 @@
 locals {
-  services = {
+  aws_services = {
+    "s3"           = "com.amazonaws.${var.aws_region}.s3"
     "eks"          = "com.amazonaws.${var.aws_region}.eks"
     "ec2"          = "com.amazonaws.${var.aws_region}.ec2"
     "ecr_api"      = "com.amazonaws.${var.aws_region}.ecr.api"
