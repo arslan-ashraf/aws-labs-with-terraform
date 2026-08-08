653ab72f6bb367ab133ea8d9f23290dd143057c6
arslan-ashraf <arslanash@gmail.com>
2026-08-08 14:49:53 -0400

    changes

---
+++ 40-secure-eks-cluster-with-vpc-endpoints/04-vpc-endpoints.tf
@@ -0,0 +1,31 @@
+# S3 Gateway Endpoint (Free & mandatory for ECR layer downloads)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+}
+
+
+locals {
+  aws_services = {
+    "eks"          = "com.amazonaws.${var.aws_region}.eks"
+    "ec2"          = "com.amazonaws.${var.aws_region}.ec2"
+    "ecr_api"      = "com.amazonaws.${var.aws_region}.ecr.api"
+    "ecr_dkr"      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+    "sts"          = "com.amazonaws.${var.aws_region}.sts"
+    "logs"         = "com.amazonaws.${var.aws_region}.logs"
+    "ssm"          = "com.amazonaws.${var.aws_region}.ssm"
+    "autoscaling"  = "com.amazonaws.${var.aws_region}.autoscaling"
+  }
+}
+
+resource "aws_vpc_endpoint" "interfaces" {
+  for_each            = local.services
+  vpc_id              = aws_vpc.eks_vpc.id
+  service_name        = each.value
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+}
