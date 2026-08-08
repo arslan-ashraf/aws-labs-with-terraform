87a44cb503246bedaa4ddf6758e51ef2288a5768
arslan-ashraf <arslanash@gmail.com>
2026-08-08 14:46:26 -0400

    changes

--- 40-secure-eks-cluster-with-vpc-endpoints/04-vpc-endpoint-names.tf
+++ 40-secure-eks-cluster-with-vpc-endpoints/04-vpc-endpoint-names.tf
@@ -1,12 +1,12 @@
 locals {
   services = {
-    "eks"          = "com.amazonaws.us-east-1.eks"
-    "ec2"          = "com.amazonaws.us-east-1.ec2"
-    "ecr_api"      = "com.amazonaws.us-east-1.ecr.api"
-    "ecr_dkr"      = "com.amazonaws.us-east-1.ecr.dkr"
-    "sts"          = "com.amazonaws.us-east-1.sts"
-    "logs"         = "com.amazonaws.us-east-1.logs"
-    "ssm"          = "com.amazonaws.us-east-1.ssm"
-    "autoscaling"  = "com.amazonaws.us-east-1.autoscaling"
+    "eks"          = "com.amazonaws.${var.aws_region}.eks"
+    "ec2"          = "com.amazonaws.${var.aws_region}.ec2"
+    "ecr_api"      = "com.amazonaws.${var.aws_region}.ecr.api"
+    "ecr_dkr"      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+    "sts"          = "com.amazonaws.${var.aws_region}.sts"
+    "logs"         = "com.amazonaws.${var.aws_region}.logs"
+    "ssm"          = "com.amazonaws.${var.aws_region}.ssm"
+    "autoscaling"  = "com.amazonaws.${var.aws_region}.autoscaling"
   }
 }
