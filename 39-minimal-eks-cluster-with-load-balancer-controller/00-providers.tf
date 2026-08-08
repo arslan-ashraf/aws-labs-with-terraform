fc1ae1af568843f674625568e41429d49de9e626
arslan-ashraf <arslanash@gmail.com>
2026-08-08 10:03:44 -0400

    changes

--- 39-minimal-eks-cluster-with-load-balancer-controller/00-providers.tf
+++ 39-minimal-eks-cluster-with-load-balancer-controller/00-providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.55.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.2.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.2.0"
+    }
   }
 }
 
