2b86f984121abccdba38ca0526b58be0c90083c7
arslan-ashraf <arslanash@gmail.com>
2026-08-08 17:41:14 -0400

    changes

---
+++ 39-minimal-eks-cluster-with-load-balancer-controller-custom-domain/17-pod-identity-association-external-dns.tf
@@ -0,0 +1,6 @@
+# resource "aws_eks_pod_identity_association" "externaldns" {
+#   cluster_name    = aws_eks_cluster.example_eks_cluster.name
+#   namespace       = "external-dns"
+#   service_account = "external-dns"
+#   role_arn        = aws_iam_role.external_dns_role.arn
+# }
