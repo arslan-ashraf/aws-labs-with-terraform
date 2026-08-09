fda7ee9b0c17ac11a233145bf6646e2aa285589d
arslan-ashraf <arslanash@gmail.com>
2026-08-08 20:26:27 -0400

    changes

--- 40-secure-eks-cluster-with-vpc-endpoints/09-eks-node-group.tf
+++ 40-secure-eks-cluster-with-vpc-endpoints/09-eks-node-group.tf
@@ -1,12 +1,16 @@
-resource "aws_eks_node_group" "nodes" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "isolated-nodes"
-  node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+resource "aws_eks_node_group" "eks_worker_nodes" {
+  cluster_name    = aws_eks_cluster.example_eks_cluster.name
+  node_group_name = "example-eks-cluster-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  
+  subnet_ids      = [
+    aws_subnet.private_subnet_1.id, 
+    aws_subnet.private_subnet_2.id
+  ]
 
   scaling_config {
     desired_size = 1
-    max_size     = 3
+    max_size     = 2
     min_size     = 1
   }
 
