f5fd0beb98d8451eb5a283f9bfc83ebced043826
arslan-ashraf <arslanash@gmail.com>
2026-08-08 20:01:56 -0400

    changes

--- 40-secure-eks-cluster-with-vpc-endpoints/iam.tf
+++ 40-secure-eks-cluster-with-vpc-endpoints/iam.tf
@@ -1,27 +1,3 @@
-data "aws_iam_policy_document" "eks_trust_policy_document" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name               = "eks_cluster_role"
-  assume_role_policy = data.aws_iam_policy_document.eks_trust_policy_document.json
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam:aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
 # Worker Node Group Role
 resource "aws_iam_role" "node" {
   name = "eks-isolated-node-role"
