07f167cd92810cede0e124046ccf2df4ab40bac6
arslan-ashraf <arslanash@gmail.com>
2026-08-15 11:30:38 -0400

    changes

--- 42-eks-cluster-with-nat-gateway/terraform-vpc-network/module/04-nat-gateway.tf
+++ 42-eks-cluster-with-nat-gateway/terraform-vpc-network/module/04-nat-gateway.tf
@@ -1,37 +1,19 @@
-resource "aws_instance" "nat_instance" {
-  availability_zone = values(local.public_subnets_for_NAT_instance)[0].AZ
-  ami               = var.NAT_instance_config.ami
-  instance_type     = var.NAT_instance_config.instance_type
-  subnet_id = aws_subnet.subnets_in_main_vpc[
-    keys(local.public_subnets_for_NAT_instance)[0]
-  ].id
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.public_SSH_key.key_name
-  user_data                   = file("${path.module}/nat_instance_user_data.sh")
+# allocate elastic IP (EIP) for the nat gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.main_vpc_internet_gateway]
 
-  # without source_dest_check = false, this EC2 instance won't be 
-  # able to serve as a NAT device and will simply drop packets coming 
-  # its way instead of forwarding them outbound to the internet gateway
-  source_dest_check = false
-
-  vpc_security_group_ids = [
-    aws_security_group.security_group_for_NAT_instance.id
-  ]
-
-  tags = { Name = "NAT-Instance", AMI = "ubuntu-ami" }
+  tags = { Name = "nat_gateway_elastic_ip" }
 }
 
-resource "aws_eip" "NAT_elastic_IP" {
-  domain = "vpc"
-  tags   = { Name = "NAT_elastic_IP" }
-}
+# create a single NAT Gateway in any public subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  public_subnets = keys(local.public_subnets)
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.subnets_in_main_vpc[public_subnets[0]].id
 
-resource "aws_eip_association" "nat" {
-  allocation_id = aws_eip.NAT_elastic_IP.id
-  instance_id   = aws_instance.nat_instance.id
-}
+  tags = { Name = "nat_gateway_in_public_subnet" }
 
-resource "aws_key_pair" "public_SSH_key" {
-  key_name   = "key-for-ec2-connection"
-  public_key = file("~/.ssh/key-for-ec2-connection.pub")
+  # Explicit dependency to ensure proper ordering during creation/destruction
+  depends_on = [aws_internet_gateway.main_vpc_internet_gateway]
 }
