144e0c640b71b261cfce7643a04f3d31cefc6b3c
arslan-ashraf <arslanash@gmail.com>
2026-08-15 16:47:37 -0400

    changes

--- 36-nacl/01-vpc-network.tf
+++ 36-nacl/01-vpc-network.tf
@@ -3,38 +3,38 @@ resource "aws_vpc" "example_vpc" {
   tags       = { Name = "example_vpc" }
 }
 
-resource "aws_internet_gateway" "internet_gateway_for_example_vpc" {
+resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.example_vpc.id
 
-  tags = { Name = "internet_gateway_for_example_vpc" }
+  tags = { Name = "internet_gateway" }
 
 }
 
-resource "aws_subnet" "public_subnet_in_example_vpc" {
+resource "aws_subnet" "public_subnet" {
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
   vpc_id            = aws_vpc.example_vpc.id
 
-  tags = { Name = "public_subnet_in_example_vpc" }
+  tags = { Name = "public_subnet" }
 }
 
 # create a route table and attach it to the internet gateway for all
 # inbound and outbound traffic
-resource "aws_route_table" "route_table_for_public_subnet_in_example_vpc" {
+resource "aws_route_table" "route_table_for_public_subnet" {
   vpc_id = aws_vpc.example_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway_for_example_vpc.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags = { Name = "route_table_for_public_subnet_in_example_vpc" }
+  tags = { Name = "route_table_for_public_subnet" }
 
 }
 
 # attach the route table to one of the subnets to make it public
 # note: a subnet can only be attached to a single route table
-resource "aws_route_table_association" "route_table_association_public_subnet_example_vpc" {
-  subnet_id      = aws_subnet.public_subnet_in_example_vpc.id
-  route_table_id = aws_route_table.route_table_for_public_subnet_in_example_vpc.id
+resource "aws_route_table_association" "public_subnet_route_table_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table_for_public_subnet.id
 }
