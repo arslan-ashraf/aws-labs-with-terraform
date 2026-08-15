df4c1628f229499df2660b138a115336617a0c57
arslan-ashraf <arslanash@gmail.com>
2026-08-15 14:59:26 -0400

    changes

--- 42-eks-cluster-with-nat-gateway/terraform-vpc-network/02-outputs.tf
+++ 42-eks-cluster-with-nat-gateway/terraform-vpc-network/02-outputs.tf
@@ -6,11 +6,20 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   value       = module.vpc_network_module.public_subnet_ids
+  description = "IDs of public subnets for ALB, NLB, etc."
+}
+
+output "public_subnets" {
+  value = module.vpc_network_module.public_subnets
   description = "Public subnets for ALB, NLB, etc."
 }
 
-
 output "private_subnet_ids" {
   value       = module.vpc_network_module.private_subnet_ids
+  description = "IDs of private subnets for EKS worker nodes"
+}
+
+output "private_subnet" {
+  value       = module.vpc_network_module.private_subnets
   description = "Private subnets for EKS worker nodes"
 }
