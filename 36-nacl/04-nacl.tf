72a8f556c7abac5b4de60c964db44e214b6a5228
arslan-ashraf <arslanash@gmail.com>
2026-08-15 16:48:16 -0400

    changes

--- 36-nacl/04-nacl.tf
+++ 36-nacl/04-nacl.tf
@@ -1,9 +1,9 @@
 # Create the Network ACL
-resource "aws_network_acl" "acl_for_public_subnet" {
+resource "aws_network_acl" "nacl_for_public_subnet" {
   vpc_id     = aws_vpc.example_vpc.id
   subnet_ids = [aws_subnet.public_subnet.id]
 
   tags = {
-    Name = "main-public-nacl"
+    Name = "nacl_for_public_subnet"
   }
 }
