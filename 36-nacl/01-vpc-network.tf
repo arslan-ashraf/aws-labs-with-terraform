dbec307b3f5398a447ad7c515f85d698c6321947
arslan-ashraf <arslanash@gmail.com>
2026-08-15 15:48:44 -0400

    changes

--- 36-nacl/01-vpc-network.tf
+++ 36-nacl/01-vpc-network.tf
@@ -12,7 +12,7 @@ resource "aws_internet_gateway" "internet_gateway_for_example_vpc" {
 
 resource "aws_subnet" "public_subnet_in_example_vpc" {
   availability_zone = "us-east-1a"
-  cidr_block        = "10.0.5.0/24"
+  cidr_block        = "10.0.1.0/24"
   vpc_id            = aws_vpc.example_vpc.id
 
   tags = { Name = "public_subnet_in_example_vpc" }
