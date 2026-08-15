d3f7f4dad3d94d2c15c961e3f563bf63a364ebfb
arslan-ashraf <arslanash@gmail.com>
2026-08-15 16:53:19 -0400

    changes

---
+++ 36-nacl/06-nacl-outbound-rules.tf
@@ -0,0 +1,12 @@
+# allow outbound traffic on ephemeral ports, this is for
+# return traffic for client requests
+resource "aws_network_acl_rule" "allow_ephemeral_outbound" {
+  network_acl_id = aws_network_acl.main.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
