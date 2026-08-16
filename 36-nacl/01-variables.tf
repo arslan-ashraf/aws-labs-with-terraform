8673f8552d0be14fc685a2e0dae12c44aa534368
arslan-ashraf <arslanash@gmail.com>
2026-08-15 20:46:41 -0400

    changes

--- 36-nacl/01-variables.tf
+++ 36-nacl/01-variables.tf
@@ -1,9 +1,9 @@
 variable "ipv4_address_to_block" {
-  type = string
+  type    = string
   default = "142.251.41.174"
 }
 
 variable "ipv6_address_to_block" {
-  type = string
+  type    = string
   default = "2607:f8b0:4006:816::200e"
 }
