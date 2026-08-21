10e1d90452cd4cd02f2f7c519fc3e551f87c133d
arslan-ashraf <arslanash@gmail.com>
2026-08-21 19:19:26 -0400

    changes

--- 36-nacl/03-security-group.tf
+++ 36-nacl/03-security-group.tf
@@ -11,13 +11,6 @@ resource "aws_vpc_security_group_egress_rule" "egress_internet_ipv4_rule" {
   ip_protocol = "-1"
 }
 
-resource "aws_vpc_security_group_egress_rule" "egress_internet_ipv6_rule" {
-  security_group_id = aws_security_group.security_group_public_traffic.id
-  cidr_ipv6         = "::/0" # where the traffic is going
-
-  ip_protocol = "-1"
-}
-
 # allow SSH into the instance
 resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
   security_group_id = aws_security_group.security_group_public_traffic.id
