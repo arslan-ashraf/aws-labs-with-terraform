3a84c533ce8d21c5cb68396de8532c1d861a0544
arslan-ashraf <arslanash@gmail.com>
2026-08-21 19:18:36 -0400

    changes

--- 30-secure-secrets-access-in-python-app-iam-role/02-security-group.tf
+++ 30-secure-secrets-access-in-python-app-iam-role/02-security-group.tf
@@ -26,16 +26,9 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_python_server_rule" {
   ip_protocol = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "egress_to_internet_ipv4_rule" {
+resource "aws_vpc_security_group_egress_rule" "egress_to_internet_rule" {
   security_group_id = aws_security_group.security_group_public_traffic.id
   cidr_ipv4         = "0.0.0.0/0" # where is the traffic going
-
-  ip_protocol = "-1"
-}
-
-resource "aws_vpc_security_group_egress_rule" "egress_internet_ipv6_rule" {
-  security_group_id = aws_security_group.security_group_public_traffic.id
-  cidr_ipv6         = "::/0" # where the traffic is going
 
   ip_protocol = "-1"
 }
