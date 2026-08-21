94ecb11b55e0dc0ab2333a0ba68c20a2be9a4281
arslan-ashraf <arslanash@gmail.com>
2026-08-21 17:26:14 -0400

    changes

---
+++ 43-minimal-eks-cluster-karpenter-on-demand-nodepool/13-k8s-in-terraform-on-demand-nodepool.tf
@@ -0,0 +1,75 @@
+resource "kubernetes_manifest" "on_demand_nodepool" {
+
+  depends_on = [
+    kubernetes_manifest.default_ec2nodeclass
+  ]
+  
+  manifest = {
+    apiVersion = "karpenter.sh/v1"
+    kind       = "NodePool"
+
+    metadata = {
+      name = "on_demand_nodepool"
+    }
+
+    spec = {
+      template = {
+        spec = {
+          nodeClassRef = {
+            group = "karpenter.k8s.aws"
+            kind  = "EC2NodeClass"
+            name  = "example_ec2nodeclass"
+          }
+
+          taints        = []
+          startupTaints = []
+
+          requirements = [
+            {
+              key      = "kubernetes.io/arch"
+              operator = "In"
+              values   = ["amd64"]
+            },
+            {
+              key      = "kubernetes.io/os"
+              operator = "In"
+              values   = ["linux"]
+            },
+            {
+              key      = "karpenter.sh/capacity-type"
+              operator = "In"
+              values   = ["on-demand"]
+            },
+            {
+              key      = "karpenter.k8s.aws/instance-family"
+              operator = "In"
+              values   = ["t3", "t3a"]
+            },
+            {
+              key      = "karpenter.k8s.aws/instance-size"
+              operator = "In"
+              values   = ["micro", "small", "medium", "large"]
+            },
+            {
+              key      = "topology.kubernetes.io/zone"
+              operator = "In"
+              values   = [
+                "us-east-1a",
+                "us-east-1b"
+              ]
+            }
+          ]
+        }
+      }
+
+      limits = {
+        cpu = "50"
+      }
+
+      disruption = {
+        consolidationPolicy = "WhenEmptyOrUnderutilized"
+        consolidateAfter    = "30s"
+      }
+    }
+  }
+}
