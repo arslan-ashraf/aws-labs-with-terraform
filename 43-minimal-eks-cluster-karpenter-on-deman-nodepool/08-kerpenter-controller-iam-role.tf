57c75273de70b837df22dc6afad3c3fe7d343b35
arslan-ashraf <arslanash@gmail.com>
2026-08-21 15:53:40 -0400

    changes

---
+++ 43-minimal-eks-cluster-karpenter-on-deman-nodepool/08-kerpenter-controller-iam-role.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "karpenter_controller_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]  
+  }
+}
+
+resource "aws_iam_role" "karpenter_controller_role" {
+  name               = "karpenter_controller_role"
+  assume_role_policy = data.aws_iam_policy_document.karpenter_controller_assume.json
+}
