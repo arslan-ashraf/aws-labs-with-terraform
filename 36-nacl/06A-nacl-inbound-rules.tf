b8d7fd0c032fb9b85cba60b578ac515df0d92c5c
arslan-ashraf <arslanash@gmail.com>
2026-08-21 22:27:19 -0400

    changes

--- 36-nacl/06A-nacl-inbound-rules.tf
+++ 36-nacl/06A-nacl-inbound-rules.tf
@@ -1,7 +1,29 @@
+# resource "aws_network_acl_rule" "block_ipv4_inbound" {
+#   network_acl_id = aws_network_acl.nacl_for_public_subnet.id
+#   rule_number    = 100
+#   egress         = false
+#   protocol       = "tcp"
+#   rule_action    = "deny"
+#   cidr_block     = "${var.ipv4_address_to_block}/32"
+#   from_port      = 0
+#   to_port        = 65535
+# }
+
+# resource "aws_network_acl_rule" "block_ipv6_inbound" {
+#   network_acl_id  = aws_network_acl.nacl_for_public_subnet.id
+#   rule_number     = 300
+#   egress          = false
+#   protocol        = "tcp"
+#   rule_action     = "deny"
+#   ipv6_cidr_block = "${var.ipv6_address_to_block}/128"
+#   from_port       = 0
+#   to_port         = 65535
+# }
+
 # allow inbound HTTP traffic on port 80
 resource "aws_network_acl_rule" "allow_http_inbound" {
   network_acl_id = aws_network_acl.nacl_for_public_subnet.id
-  rule_number    = 100
+  rule_number    = 500
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -13,7 +35,7 @@ resource "aws_network_acl_rule" "allow_http_inbound" {
 # allow inbound HTTPS traffic on port 443
 resource "aws_network_acl_rule" "allow_https_inbound" {
   network_acl_id = aws_network_acl.nacl_for_public_subnet.id
-  rule_number    = 300
+  rule_number    = 700
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -24,35 +46,13 @@ resource "aws_network_acl_rule" "allow_https_inbound" {
 
 resource "aws_network_acl_rule" "allow_ssh_inbound" {
   network_acl_id = aws_network_acl.nacl_for_public_subnet.id
-  rule_number    = 500
+  rule_number    = 900
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
   from_port      = 22
   to_port        = 22
-}
-
-resource "aws_network_acl_rule" "block_ipv4_inbound" {
-  network_acl_id = aws_network_acl.nacl_for_public_subnet.id
-  rule_number    = 700
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "deny"
-  cidr_block     = "${var.ipv4_address_to_block}/32"
-  from_port      = 0
-  to_port        = 65535
-}
-
-resource "aws_network_acl_rule" "block_ipv6_inbound" {
-  network_acl_id  = aws_network_acl.nacl_for_public_subnet.id
-  rule_number     = 900
-  egress          = false
-  protocol        = "tcp"
-  rule_action     = "deny"
-  ipv6_cidr_block = "${var.ipv6_address_to_block}/128"
-  from_port       = 0
-  to_port         = 65535
 }
 
 resource "aws_network_acl_rule" "allow_ephemeral_inbound" {
