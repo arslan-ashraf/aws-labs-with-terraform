6ffdb3cbdb6ffd2d8db2e2ce878d82bf98ee147c
arslan-ashraf <arslanash@gmail.com>
2026-08-22 12:18:54 -0400

    changes

--- 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/02C-karpenter-pod-identity-association.tf
+++ 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/02C-karpenter-pod-identity-association.tf
@@ -4,7 +4,7 @@
 # 3. the correct k8s service account in the correct namespace
 
 resource "aws_eks_pod_identity_association" "karpenter_PIA" {
-  cluster_name    = var.eks_cluster_name
+  cluster_name    = local.eks_cluster_name
   namespace       = "kube-system"
   service_account = "karpenter-sa"
   role_arn        = aws_iam_role.karpenter_controller_role.arn
--- 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/02E-karpenter-eks-access-entry.tf
+++ 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/02E-karpenter-eks-access-entry.tf
@@ -1,10 +1,9 @@
 resource "aws_eks_access_entry" "karpenter_node_access" {
-  cluster_name  = var.eks_cluster_name
+  cluster_name  = local.eks_cluster_name
   principal_arn = aws_iam_role.karpenter_node_role.arn
   type          = "EC2_LINUX"
 
   depends_on = [
-    aws_eks_cluster.example_eks_cluster,
     aws_eks_pod_identity_association.karpenter_PIA,
     aws_iam_role.karpenter_node_role
   ]
