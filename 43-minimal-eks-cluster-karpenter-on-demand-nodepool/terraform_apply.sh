590b096834ea19705999cb660bff1100e49286f7
arslan-ashraf <arslanash@gmail.com>
2026-08-22 17:00:17 -0400

    changes

--- 43-minimal-eks-cluster-karpenter-on-demand-nodepool/terraform_apply.sh
+++ 43-minimal-eks-cluster-karpenter-on-demand-nodepool/terraform_apply.sh
@@ -4,6 +4,8 @@ cd 01-terraform-eks-cluster
 
 terraform init
 
+terraform fmt -recursive
+
 terraform apply -auto-approve
 
 aws eks update-kubeconfig --region us-east-1 --name example_eks_cluster
@@ -12,4 +14,14 @@ aws eks update-kubeconfig --region us-east-1 --name example_eks_cluster
 
 # terraform init
 
+# terraform fmt -recursive
+
 # terraform apply -auto-approve
+
+# cd ../03-k8s-in-terraform
+
+# terraform init
+
+# terraform fmt -recursive
+
+# terraform apply auto-approve
