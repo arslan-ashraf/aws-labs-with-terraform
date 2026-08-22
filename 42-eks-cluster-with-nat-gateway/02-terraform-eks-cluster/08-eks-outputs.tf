e42574a701e92eb5f5d1b44e2e0c946fe90d5d94
arslan-ashraf <arslanash@gmail.com>
2026-08-22 16:11:28 -0400

    changes

--- 42-eks-cluster-with-nat-gateway/02-terraform-eks-cluster/08-eks-outputs.tf
+++ 42-eks-cluster-with-nat-gateway/02-terraform-eks-cluster/08-eks-outputs.tf
@@ -1,8 +1,3 @@
-output "command_to_configure_kubectl" {
-  description = "Command to update local kubeconfig to connect to the EKS cluster"
-  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
-}
-
 # EKS Cluster API server endpoint
 # used by kubectl and external tools to communicate with the cluster
 output "eks_cluster_endpoint" {
@@ -32,4 +27,9 @@ output "eks_cluster_name" {
 output "eks_cluster_certificate_authority_data" {
   value       = aws_eks_cluster.example_eks_cluster.certificate_authority[0].data
   description = "Base64 encoded CA certificate for kubectl config"
+}
+
+output "command_to_configure_kubectl" {
+  description = "Command to update local kubeconfig to connect to the EKS cluster"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${var.eks_cluster_name}"
 }
