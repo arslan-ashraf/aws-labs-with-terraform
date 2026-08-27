0ecd041da63b18fc8ea7f026930d42458db4d664
arslan-ashraf <arslanash@gmail.com>
2026-08-27 13:24:28 -0400

    changes

--- 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/01-helm-provider.tf
+++ 43-minimal-eks-cluster-karpenter-on-demand-nodepool/02-terraform-karpenter/01-helm-provider.tf
@@ -5,11 +5,6 @@ terraform {
       version = "~> 6.55.0"
     }
 
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = "~> 3.2.0"
-    # }
-
     helm = {
       source  = "hashicorp/helm"
       version = "~> 3.2.0"
@@ -35,14 +30,3 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
-
-
-# connect Kubernetes Terraform API to EKS cluster
-# allows creation of Kubernetes resources using Terraform instead
-# of yaml files, this is only a best practice when Terraform results
-# need to be dynamically injected into a Kubernetes resource
-# provider "kubernetes" {
-#   host                   = local.eks_cluster_endpoint
-#   cluster_ca_certificate = base64decode(local.certificate_authority)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
