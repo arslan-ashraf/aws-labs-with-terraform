e79e68380877c2a7d1d8db3e0d195da5dc95b868
arslan-ashraf <arslanash@gmail.com>
2026-08-27 17:29:09 -0400

    changes

--- 45-minimal-eks-cluster-karpenter-spot-nodepool-PDB/02-terraform-karpenter/06-karpenter-event-bridge-rules.tf
+++ 45-minimal-eks-cluster-karpenter-spot-nodepool-PDB/02-terraform-karpenter/06-karpenter-event-bridge-rules.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_event_rule" "karpenter_health_event" {
   tags = { Name = "${local.eks_cluster_name}-aws-health-events" }
 }
 
-resource "aws_cloudwatch_event_target" "karpenter_health_target" {
+resource "aws_cloudwatch_event_target" "karpenter_health_event_target" {
   rule      = aws_cloudwatch_event_rule.karpenter_health_event.name
   target_id = "KarpenterHealthTarget"
   arn       = aws_sqs_queue.karpenter_ec2_spot_interruption_queue.arn
@@ -27,7 +27,7 @@ resource "aws_cloudwatch_event_rule" "karpenter_ec2_spot_interrupt" {
   tags = { Name = "${local.eks_cluster_name}-ec2-spot-interruption-warning" }
 }
 
-resource "aws_cloudwatch_event_target" "karpenter_spot_target" {
+resource "aws_cloudwatch_event_target" "karpenter_ec2_spot_target" {
   rule      = aws_cloudwatch_event_rule.karpenter_ec2_spot_interrupt.name
   target_id = "KarpenterSpotTarget"
   arn       = aws_sqs_queue.karpenter_ec2_spot_interruption_queue.arn
@@ -45,29 +45,26 @@ resource "aws_cloudwatch_event_rule" "karpenter_ec2_instance_rebalance" {
   tags = { Name = "${local.eks_cluster_name}_karpenter_ec2_instance_rebalance" }
 }
 
-resource "aws_cloudwatch_event_target" "karpenter_rebalance_target" {
+resource "aws_cloudwatch_event_target" "karpenter_ec2_rebalance_target" {
   rule      = aws_cloudwatch_event_rule.karpenter_ec2_instance_rebalance.name
   target_id = "KarpenterRebalanceTarget"
   arn       = aws_sqs_queue.karpenter_ec2_spot_interruption_queue.arn
 }
 
 
-# EC2 Instance State-change Notification → SQS
-
-resource "aws_cloudwatch_event_rule" "karpenter_instance_state" {
-  name        = "${local.short_cluster}-k-state"
-  description = "EC2 Instance State Change Notification → Karpenter SQS Queue"
-
+# EC2 instance state change notification
+resource "aws_cloudwatch_event_rule" "karpenter_ec2_instance_state" {
+  name        = "${local.eks_cluster_name}_karpenter_ec2_instance_state"
   event_pattern = jsonencode({
     source       = ["aws.ec2"]
     "detail-type" = ["EC2 Instance State-change Notification"]
   })
 
-  tags = var.tags
+  tags = { Name = "${local.eks_cluster_name}_karpenter_ec2_instance_state" }
 }
 
-resource "aws_cloudwatch_event_target" "karpenter_instance_state_target" {
-  rule      = aws_cloudwatch_event_rule.karpenter_instance_state.name
+resource "aws_cloudwatch_event_target" "karpenter_ec2_instance_state_target" {
+  rule      = aws_cloudwatch_event_rule.karpenter_ec2_instance_state.name
   target_id = "KarpenterStateTarget"
   arn       = aws_sqs_queue.karpenter_ec2_spot_interruption_queue.arn
 }
