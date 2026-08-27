7bb70e4eca9ca8821fd2ab6b5a3cda3610d54a19
arslan-ashraf <arslanash@gmail.com>
2026-08-27 15:53:57 -0400

    changes

---
+++ 45-minimal-eks-cluster-karpenter-spot-nodepool-PDB/02-terraform-karpenter/05-karpenter-sqs-queue.tf
@@ -0,0 +1,38 @@
+resource "aws_sqs_queue" "ec2_spot_interruption_queue" {
+  name                      = "ec2_spot_interruption_queue_for_${local.eks_cluster_name}"
+  
+  # EC2 spot interruption is at most 2 minutes (120 seconds)
+  # and then AWS will take those instances back, so we must respond
+  # in at most 120 seconds or else the message is meaningless
+  message_retention_seconds = 120 # seconds
+  sqs_managed_sse_enabled   = true
+}
+
+resource "aws_sqs_queue_policy" "ec2_spot_interruption_queue" {
+  queue_url = aws_sqs_queue.ec2_spot_interruption_queue.url
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = ["events.amazonaws.com", "sqs.amazonaws.com"]
+        }
+        Action   = "sqs:SendMessage"
+        Resource = aws_sqs_queue.ec2_spot_interruption_queue.arn
+      },
+      {
+        Sid      = "DenyHTTP"
+        Effect   = "Deny"
+        Action   = "sqs:*"
+        Resource = aws_sqs_queue.ec2_spot_interruption_queue.arn
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+        Principal = "*"
+      }
+    ]
+  })
+}
