b35bbb159632d67a1a07e253931e9e016b996d9c
arslan-ashraf <arslanash@gmail.com>
2026-08-27 16:30:53 -0400

    changes

--- 45-minimal-eks-cluster-karpenter-spot-nodepool-PDB/02-terraform-karpenter/05-karpenter-sqs-queue.tf
+++ 45-minimal-eks-cluster-karpenter-spot-nodepool-PDB/02-terraform-karpenter/05-karpenter-sqs-queue.tf
@@ -24,6 +24,14 @@ data "aws_iam_policy_document" "sqs_resource_policy" {
       identifiers = ["events.amazonaws.com"]
     }
 
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [
+        aws_cloudwatch_event_rule.karpenter_spot_interruption.arn
+      ]
+    }
+
   }
 
   statement {
