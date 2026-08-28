c8f97c3c966bb8d7fa000dec33764cdee9f24bab
arslan-ashraf <arslanash@gmail.com>
2026-08-28 19:08:31 -0400

    changes

--- 46-minimal-eks-cluster-karpenter-horizontal-pod-autoscaler/terraform_apply.sh
+++ 46-minimal-eks-cluster-karpenter-horizontal-pod-autoscaler/terraform_apply.sh
@@ -4,18 +4,18 @@ cd 01-terraform-eks-cluster
 
 terraform init
 
-terraform apply -auto-approve
+# terraform apply -auto-approve
 
-aws eks update-kubeconfig --region us-east-1 --name example_eks_cluster
+# aws eks update-kubeconfig --region us-east-1 --name example_eks_cluster
 
 cd ../02-terraform-karpenter
 
 terraform init
 
-terraform apply -auto-approve
+# terraform apply -auto-approve
 
 cd ../03-k8s-in-terraform
 
 terraform init
 
-terraform apply -auto-approve
+# terraform apply -auto-approve
