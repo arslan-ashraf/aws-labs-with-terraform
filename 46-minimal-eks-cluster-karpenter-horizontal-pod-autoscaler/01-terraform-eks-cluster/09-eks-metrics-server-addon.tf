0f1fde16746ca265dc42545a546b38b813dc6972
arslan-ashraf <arslanash@gmail.com>
2026-08-29 10:54:44 -0400

    changes

---
+++ 46-minimal-eks-cluster-karpenter-horizontal-pod-autoscaler/01-terraform-eks-cluster/09-eks-metrics-server-addon.tf
@@ -0,0 +1,22 @@
+# for learning only, we use the latest version
+# get EKS addon version compatible with current EKS cluster version
+data "aws_eks_addon_version" "metrics_server_default" {
+  addon_name         = "metrics-server"
+  kubernetes_version = var.kubernetes_version
+}
+
+# get latest EKS addon version compatible with current EKS cluster version
+data "aws_eks_addon_version" "metrics_server_latest" {
+  addon_name         = "metrics-server"
+  kubernetes_version = var.kubernetes_version
+  most_recent        = true
+}
+
+resource "aws_eks_addon" "metrics_server" {
+  cluster_name                = var.eks_cluster_name
+  addon_name                  = "metrics-server"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  # Use the latest EKS addon version compatible with the cluster's Kubernetes version
+  addon_version               = data.aws_eks_addon_version.metrics_server_latest.version
+}
