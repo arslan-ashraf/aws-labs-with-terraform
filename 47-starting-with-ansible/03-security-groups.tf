d49aece8f543ca5955d510f7083ce397310861fd
arslan-ashraf <arslanash@gmail.com>
2026-09-19 22:05:32 -0400

    changes

--- 47-starting-with-ansible/03-security-groups.tf
+++ 47-starting-with-ansible/03-security-groups.tf
@@ -30,11 +30,13 @@ resource "aws_vpc_security_group_egress_rule" "egress_internet_rule" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_connection_rule_private_sg" {
-  description = "Allow only ssh connection on port 22 from within VPC"
+  description = "Allow only SSH connection on port 22 from within VPC"
 
   security_group_id = aws_security_group.multiple_security_groups["private_traffic_sg"].id
 
-  cidr_ipv4   = "10.0.0.0/16" # where is the traffic coming from, from within VPC
+  # where the traffic is coming from, from within the VPC
+  cidr_ipv4   = aws_vpc.example_vpc.cidr_block
+
   from_port   = 22
   to_port     = 22
   ip_protocol = "tcp"
